17234f3a9d692bc270998368a9b6611e5e02f378
Gernot Kapper <gernot.kapper@tugraz.at>
2014-03-25 18:58:02 +0100

    New Testcases New Script directory

---
+++ Scripts/do_versioning.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+GITHASH=`git rev-parse HEAD`
+GITSHORTHASH=`git rev-parse --short HEAD`
+GITCHANGEDFILES=`git diff-index --name-only HEAD`
+
+export GITSHORTHASH=$GITSHORTHASH
+
+echo "character(len=*), parameter :: Neo2_Version = '${GITHASH}'" > ./version.f90
+echo "Versioning NEO-2..."
+
+if [ -n "$GITCHANGEDFILES" ]; then
+    echo 'character(len=*), parameter :: Neo2_Version_Additional = "WARNING, &' >> ./version.f90
+    echo "&THERE ARE UNCOMMITTED CHANGES. Run may not be reproduceable: &" >> ./version.f90
+
+    while read -r line; do
+        echo "&${line} &" >> ./version.f90
+    done <<< "$GITCHANGEDFILES"
+    echo '&"' >> ./version.f90
+
+fi
