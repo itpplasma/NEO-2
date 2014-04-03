02cb204c72329f20cda4ea17ef41269ba4b89b4b
TU Graz ITPcp Plasma <info@tugraz.at>
2014-04-03 18:20:52 +0200

    Show Git Hash when make is called

--- Scripts/tag_version.sh
+++ Scripts/tag_version.sh
@@ -6,12 +6,14 @@ GITCHANGEDFILES=`git diff-index --name-only HEAD`
 export GITSHORTHASH=$GITSHORTHASH
 
 echo "character(len=*), parameter :: Neo2_Version = '${GITHASH}'" > ./version.f90
-echo "Versioning NEO-2..."
+echo ""
+echo "Git Hash is ${GITHASH}"
 
 if [ -n "$GITCHANGEDFILES" ]; then
     echo 'character(len=*), parameter :: Neo2_Version_Additional = "WARNING, &' >> ./version.f90
     echo "&THERE ARE UNCOMMITTED CHANGES. Run may not be reproduceable: &" >> ./version.f90
-
+    echo "THERE ARE UNCOMMITTED CHANGES!"
+    echo ""
     while read -r line; do
         echo "&${line} &" >> ./version.f90
     done <<< "$GITCHANGEDFILES"
