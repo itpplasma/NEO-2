ec005219269c0295fc02f63afffa66682dc3aa04
Gernot Kapper <gernot.kapper@tugraz.at>
2015-07-28 21:40:27 +0200

    Fixed weightlag and sorting of asource

--- collision_operator_mems.f90
+++ collision_operator_mems.f90
@@ -14,15 +14,62 @@ module collop
   logical, public :: collop_talk_much =  .true.
   character(len=100), public :: collop_path
 
+  integer, private :: collop_unit = 300
+  character(len=100), public         :: collop_file_Cm  = 'VectorCm.dat'
+  integer, public                    :: collop_C_m_min = 0    
+  integer, public                    :: collop_C_m_max = 0    
+  real(kind=dp), allocatable, public :: collop_C(:)
+  
+
   !**********************************************************
   ! Number of species
   !**********************************************************
   integer :: num_spec
+
+  ! private only
+  private full_file
+  private full_file_1
+  interface full_file
+     module procedure full_file_1
+  end interface full_file
+  
+  private check_unit
+  private check_unit_1
+  interface check_unit
+     module procedure check_unit_1
+  end interface check_unit
   
   contains
+
+    ! ---------------------------------------------------------------------------
+    ! private routines
+    ! ---------------------------------------------------------------------------
+    function full_file_1(name_in) result(name_out)
+      character(len=100), intent(in)  :: name_in
+      character(len=200) :: name_out
+      
+      write(name_out,'(200A)') trim(adjustl(collop_path)),trim(adjustl(name_in))
+      
+    end function full_file_1
+    
+    ! ---------------------------------------------------------------------------
+    subroutine check_unit_1
+      logical :: opened
+      do
+         inquire(unit=collop_unit,opened=opened)
+         if(.not. opened) exit
+         collop_unit = collop_unit + 1
+      end do
+    end subroutine check_unit_1
     
     subroutine collop_construct()     
+      use nrtype, only : pi
+      integer          :: openstatus, readstatus, k, m
+      character(len=1) :: dummy
+
       num_spec = 2
+
+      call check_unit      
       
       if(allocated(anumm_ms)) deallocate(anumm_ms)
       allocate(anumm_ms(0:lag,0:lag,0:num_spec-1))
@@ -36,6 +83,53 @@ module collop
       allocate(ailmm(0:lag,0:lag,0:leg))
       if(allocated(weightlag)) deallocate(weightlag)
       allocate(weightlag(3,0:lag))
+
+      ! open
+      open(unit=collop_unit,file=full_file(collop_file_Cm), & 
+           status='old',action='read',iostat=openstatus)
+      if (openstatus .ne. 0) then
+         print *, 'Can not open file: ',full_file(collop_file_Cm)
+         stop
+      end if
+      
+      ! read initial lines
+      do k = 1, dummy_read
+         read(collop_unit,*,iostat=readstatus) dummy
+         if (readstatus .ne. 0) then
+            print *, 'Can not read file: ',full_file(collop_file_Cm),' (initial lines)'
+            stop
+         end if
+      end do
+      
+      ! read upper size
+      read(collop_unit,*,iostat=readstatus) collop_C_m_max
+      if (readstatus .ne. 0) then
+         print *, 'Can not read file: ',full_file(collop_file_Cm),' (size)'
+         stop
+      end if
+
+      if (allocated(collop_C))  deallocate(collop_C)
+      allocate(collop_C(collop_C_m_min:collop_C_m_max))
+
+      ! read data
+      do m = collop_C_m_min,collop_C_m_max
+         read(collop_unit,*,iostat=readstatus) collop_C(m)
+         if (readstatus .ne. 0) then
+            print *, 'Can not read file: ',full_file(collop_file_Cm),' at m=',m
+            stop
+         end if
+      end do
+
+      weightlag=0.d0
+      !    weightlag(1,0)=SQRT(6.d0)*pi/4.d0               !***change: 08.08.07 =>
+      weightlag(1,0)=SQRT(6.d0*pi)/4.d0
+      !    weightlag(2,0:lag)=collop_C(0:lag)              !***change: 19.09.07
+      weightlag(2,0:lag)=collop_C(0:lag)/sqrt(pi)
+      !    weightlag(3,0)=5.d0*SQRT(6.d0*pi)/16.d0         !***change: 19.09.07
+      weightlag(3,0)=5.d0*SQRT(6.d0*pi)/8.d0
+      !    IF(lag.GT.0) weightlag(3,1)=SQRT(15.d0*pi)/8.d0 !***change: 08.08.07 =>
+      !    IF(lag.GT.0) weightlag(3,1)=-SQRT(15.d0*pi)/8.d0!***change: 19.09.07
+      IF(lag.GT.0) weightlag(3,1)=-SQRT(15.d0*pi)/4.d0
       
     end subroutine collop_construct
 
@@ -53,6 +147,7 @@ module collop
     end subroutine collop_set_species
     
     subroutine collop_load()
+      real(kind=dp), dimension(:), allocatable :: temp
 
       !**********************************************************
       ! Test configuration dd and dC
@@ -63,6 +158,12 @@ module collop
       call collop_set_species(1, .false.)
       call compute_collop('d', 'C' , m_d, m_C, 1d0, 1d0, asource, anumm, denmm, ailmm)
 
+      allocate(temp(0:lag))
+      temp = asource(:, 2)
+      asource(:,2) = asource(:,3)
+      asource(:,3) = temp
+      deallocate(temp)
+      
       !**********************************************************
       ! Set to main species
       !**********************************************************
