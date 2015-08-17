205a9367f1bc06b6f0e9621330fdd6781a884754
TU Graz ITPcp Plasma <info@tugraz.at>
2015-08-17 17:20:26 +0200

    Added include directory to CMakeLists for hdf5_tools

--- ripple_solver_ArnoldiOrder1.f90
+++ ripple_solver_ArnoldiOrder1.f90
@@ -115,7 +115,7 @@ SUBROUTINE ripple_solver_ArnoldiO1(                       &
   !! Modification by Andreas F. Martitsch (28.07.2015)
   
   IMPLICIT NONE
-  INTEGER, PARAMETER :: dp = KIND(1.0d0)
+  !INTEGER, PARAMETER :: dp = KIND(1.0d0)
   DOUBLE COMPLEX, PARAMETER :: imun=(0.d0,1.d0)
   REAL(DP), PARAMETER :: PI=3.141592653589793238462643383279502884197_dp
 
