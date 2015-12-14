7360bd3dac1d53a2acdab02fc3b4743aa3f36078
TU Graz ITPcp Plasma <info@tugraz.at>
2015-12-14 10:35:46 +0100

    Arbitrary BSpline orders with non-uniform grid distribution and Taylor expansion

--- neo_mod.f90
+++ neo_mod.f90
@@ -50,13 +50,11 @@
     DOUBLE PRECISION :: coeps
     DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:) :: y_axi_averages
     DOUBLE PRECISION, ALLOCATABLE, DIMENSION(:) :: vel_array
-    !**********************************************************
-    ! WINNY - for flint
-    double precision :: collpar_min,collpar_max,v_min_resolution,v_max_resolution
-    double precision :: phi_x_max
-    ! WINNY - for flint
-    !**********************************************************
 
+    DOUBLE PRECISION :: collpar_min,collpar_max,v_min_resolution,v_max_resolution
+    DOUBLE PRECISION :: phi_x_max
+    DOUBLE PRECISION :: collop_bspline_dist
+    INTEGER          :: collop_bspline_order
 
   END MODULE
 !
