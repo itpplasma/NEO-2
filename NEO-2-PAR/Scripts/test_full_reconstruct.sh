60ba34033edfb4d903bb7032b248c04760997f66
Rico Buchholz <buchholz@tugraz.at>
2018-10-08 10:44:43 +0200

    A first rewrite of the script.
    Remove some hardcoded username in a path. The script still expects the
    existence of some specific folders, which should be changed, i.e. they
    should be set via a config file or during a configuration stage.
    
    Remove some code doubling. The if clause now only sets runcommands and
    prints a message what is/will be done. Note that this means that for the
    parallelized case of switch_reconstruction.sh 1 now the code is run in
    parallel. So far the code was run with only one processor, i am not sure
    if this is a copy and paste error or if this was intended/required.
    There is still some code doubling that could be avoided, as the
    different runs differ just by a number.
    
    Use variables for the commandline arguments. The commandline arguments
    are written to variables, and these are used throughout the script.

--- NEO-2-PAR/Scripts/test_full_reconstruct.sh
+++ NEO-2-PAR/Scripts/test_full_reconstruct.sh
@@ -1,94 +1,60 @@
 #!/bin/bash
 
-testpath=`mktemp -d /temp/gernot_k/Neo2/Testing/Runs/Test-XXXXXX`
+testcase=${1}
+number_processors=${2}
+
+testpath=`mktemp -d /temp/$LOGNAME/Neo2/Testing/Runs/Test-XXXXXX`
 echo "Testpath created: ${testpath}!"
-#testpath='/temp/gernot_k/Neo2/Testing/Runs/000'
 
 cp ./neo_2.x $testpath
 cd $testpath
 
 echo "Copying template..."
-cp -r ../../Template/${1}/ .
+cp -r ../../Template/${testcase}/ .
 
-cd ${1}
-echo "Running Test ${1}..."
+cd ${testcase}
+echo "Running Test ${testcase}..."
 
-if [ ${2} -eq 0 ]; then
-	echo "Sequential mode"
-
-	switch_reconstruction.sh 0
-	../neo_2.x >> job.log 2>&1
-
-	if md5sum -c ../../../Reference/${1}/MD5sums-reconstruct_0 ; then
-        	echo "Test (1/3) passed. Checksums correct."
-	        #exit 0
-	else
-       		echo "Test (1/3) failed. Not all files are equal."
-        	exit 1
-	fi
-
-        switch_reconstruction.sh 1
-        ../neo_2.x >> job.log 2>&1
-
-        if md5sum -c ../../../Reference/${1}/MD5sums-reconstruct_1 ; then
-                echo "Test (2/3) passed. Checksums correct."
-                #exit 0
-        else
-                echo "Test (2/3) failed. Not all files are equal."
-                exit 1
-        fi
-
-        switch_reconstruction.sh 2
-        ../neo_2.x >> job.log 2>&1
-
-        if md5sum -c ../../../Reference/${1}/MD5sums-reconstruct_2 ; then
-                echo "Test (3/3) passed. Checksums correct."
-                #exit 0
-        else
-                echo "Test (3/3) failed. Not all files are equal."
-                exit 1
-        fi
-
+if [ ${number_processors} -eq 0 ]; then
+  echo "Sequential mode"
+  runcommand="$testpath/neo_2.x"
 else
-	echo "Parallel mode np=${2}"
+  echo "Parallel mode np=${number_processors}"
+  runcommand="mpirun -np ${number_processors} $testpath/neo_2.x"
+fi
 
-        switch_reconstruction.sh 0
-	mpirun -np ${2} ../neo_2.x >> job.log 2>&1
+switch_reconstruction.sh 0
+$runcommand >> job.log 2>&1
 
-        if md5sum -c ../../../Reference/${1}/MD5sums-reconstruct_0 ; then
-                echo "Test (1/3) passed. Checksums correct."
-                #exit 0
-        else
-                echo "Test (1/3) failed. Not all files are equal."
-                exit 1
-        fi
+if md5sum -c ../../../Reference/${testcase}/MD5sums-reconstruct_0 ; then
+  echo "Test (1/3) passed. Checksums correct."
+else
+  echo "Test (1/3) failed. Not all files are equal."
+  exit 1
+fi
 
-        switch_reconstruction.sh 1
-        ../neo_2.x >> job.log 2>&1
+switch_reconstruction.sh 1
+$runcommand >> job.log 2>&1
 
-        if md5sum -c ../../../Reference/${1}/MD5sums-reconstruct_1 ; then
-                echo "Test (2/3) passed. Checksums correct."
-                #exit 0
-        else
-                echo "Test (2/3) failed. Not all files are equal."
-                exit 1
-        fi
+if md5sum -c ../../../Reference/${testcase}/MD5sums-reconstruct_1 ; then
+  echo "Test (2/3) passed. Checksums correct."
+else
+  echo "Test (2/3) failed. Not all files are equal."
+  exit 1
+fi
 
-        switch_reconstruction.sh 2
-	mpirun -np ${2} ../neo_2.x >> job.log 2>&1
+switch_reconstruction.sh 2
+$runcommand >> job.log 2>&1
 
-        if md5sum -c ../../../Reference/${1}/MD5sums-reconstruct_2 ; then
-                echo "Test (3/3) passed. Checksums correct."
-                #exit 0
-        else
-                echo "Test (3/3) failed. Not all files are equal."
-                exit 1
-        fi
-
+if md5sum -c ../../../Reference/${testcase}/MD5sums-reconstruct_2 ; then
+  echo "Test (3/3) passed. Checksums correct."
+else
+  echo "Test (3/3) failed. Not all files are equal."
+  exit 1
 fi
 
 echo "Tests passed. No exit code 1 received."
 exit 0
 
 #Maybe dangerous...
-#rm -r $testpath
+#~ rm -r $testpath
