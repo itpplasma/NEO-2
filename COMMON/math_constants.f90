c312eb290ae7293f520abce5386a04bfb8d881d6
Rico Buchholz <buchholz@tugraz.at>
2018-10-15 17:47:44 +0200

    Add imaginary unit as math constant.
    Named 'imun' as this is already used in the code.
    
    Also add notice that this module is also for physical constants.

--- COMMON/math_constants.f90
+++ COMMON/math_constants.f90
@@ -1,5 +1,8 @@
+!> Despite the name this module contains mathematical and physical
+!> constants, as it is sometimes hard to distinguish, e.g. for
+!> conversion factors.
 module math_constants
-  use nrtype, only : DP
+  use nrtype, only : DP, dpc
 
   real(DP), parameter :: PI=3.141592653589793238462643383279502884197_dp
   real(DP), parameter :: PIO2=1.57079632679489661923132169163975144209858_dp
@@ -13,5 +16,7 @@ module math_constants
   real(DP), parameter :: PI_D=3.141592653589793238462643383279502884197_dp
   real(DP), parameter :: PIO2_D=1.57079632679489661923132169163975144209858_dp
   real(DP), parameter :: TWOPI_D=6.283185307179586476925286766559005768394_dp
+
+  complex(DPC), parameter :: IMUN = (0.d0,1.d0)
 
 end module math_constants
