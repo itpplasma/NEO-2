475df182755cb77b116f92d58998135c6d84d528
Rico Buchholz <buchholz@tugraz.at>
2018-10-15 16:44:33 +0200

    Move mathematical constants to own module.

--- COMMON/nrtype.f90
+++ COMMON/nrtype.f90
@@ -20,18 +20,6 @@ module nrtype
   integer, parameter :: DPC = kind((1.0D0,1.0D0))
   integer, parameter :: LGT = kind(.TRUE.)
 
-  real(DP), parameter :: PI=3.141592653589793238462643383279502884197_dp
-  real(DP), parameter :: PIO2=1.57079632679489661923132169163975144209858_dp
-  real(DP), parameter :: TWOPI=6.283185307179586476925286766559005768394_dp
-  real(DP), parameter :: SQRT2=1.41421356237309504880168872420969807856967_dp
-  !> Defined as \f$ \gamma = \int\limits_{1}^{\infty} 1/floor(x) - 1/x dx \f$
-  !> or alternatively \f$ \sum\limits_{k=1}^{\infty} ( 1/k - \ln ((k+1)/k)\f$
-  !> or \f$ \Gamma'(1) = - \gamma \f$.
-  real(DP), parameter :: EULER=0.5772156649015328606065120900824024310422_dp
-  real(DP), parameter :: PI_D=3.141592653589793238462643383279502884197_dp
-  real(DP), parameter :: PIO2_D=1.57079632679489661923132169163975144209858_dp
-  real(DP), parameter :: TWOPI_D=6.283185307179586476925286766559005768394_dp
-
   type sprs2_sp
      integer(I4B) :: n,len
      real(SP), dimension(:), pointer :: val
