cb0584f1cd2ad277dfe2586b536d9d1db0fe9d24
Rico Buchholz <buchholz@tugraz.at>
2018-10-19 13:50:56 +0200

    Rename subroutine parameter.
    Is used like a logical and thus was made a logical.
    Also - formatting changes.
         - only use required variables.
         - define intent for parameter.

--- COMMON/kin_allocate.f90
+++ COMMON/kin_allocate.f90
@@ -1,18 +1,17 @@
-!
-  subroutine kin_allocate(ialloc)
-!
-  use size_mod
-  use rk4_kin_mod
-!
+
+subroutine kin_allocate(lalloc)
+
+  use size_mod, only : ndim
+  use rk4_kin_mod, only : y, dydx, yt, dyt, dym
+
   implicit none
-!
-  integer :: ialloc
-!
-  if(ialloc.eq.1) then
-    allocate(y(ndim),dydx(ndim),yt(ndim),dyt(ndim),dym(ndim))
+
+  logical, intent(in) :: lalloc
+
+  if (lalloc) then
+    allocate(y(ndim), dydx(ndim), yt(ndim), dyt(ndim), dym(ndim))
   else
-    deallocate(y,dydx,yt,dyt,dym)
-  endif
-!
-  return
-  end
+    deallocate(y, dydx, yt, dyt, dym)
+  end if
+
+end subroutine kin_allocate
