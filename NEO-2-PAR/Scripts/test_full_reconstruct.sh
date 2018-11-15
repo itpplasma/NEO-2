cdaeb20bbf47a4a83673a8db7bee2ce489be3db8
Rico Buchholz <buchholz@tugraz.at>
2018-11-15 14:31:08 +0100

    Use function for comparison of dat-files.
    Does not shorten the code, but should make it more readable, and future
    improvements to the comparison might be done easier.

--- NEO-2-PAR/Scripts/test_full_reconstruct.sh
+++ NEO-2-PAR/Scripts/test_full_reconstruct.sh
@@ -12,6 +12,25 @@ referencepath='/temp/buchholz/Neo2/Testing/Reference/'
 totalnumberofstages=4
 numberofstage=0
 
+########################################################################
+function check_equality_dat {
+  referencepath_local=${1}
+  testcase_local=${2}
+  numberofstage_local=${3}
+
+  return_value_loc=0
+
+  if md5sum -c $referencepath_local/${testcase_local}/MD5sums-reconstruct_$numberofstage_local ; then
+    a='' # just to avoid reports of errors.
+  else
+    return_value_loc=1
+  fi
+
+  return $return_value_loc
+}
+
+########################################################################
+
 cp ./neo_2.x $testpath
 cd $testpath
 
@@ -32,7 +51,9 @@ fi
 for numberofstage in 0 1 2 3 ; do
   switch_reconstruction.sh $numberofstage
   $runcommand >> job.log 2>&1
-  if md5sum -c $referencepath/${testcase}/MD5sums-reconstruct_$numberofstage ; then
+  #~ if md5sum -c $referencepath/${testcase}/MD5sums-reconstruct_$numberofstage ; then
+  # check_equality_dat is a function.
+  if check_equality_dat $referencepath ${testcase} $numberofstage ; then
     echo "Test ($numberofstage/$totalnumberofstages) passed. Checksums correct."
   else
     echo "Test ($numberofstage/$totalnumberofstages) failed. Not all files are equal."
