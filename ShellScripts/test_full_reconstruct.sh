63e864d42eea99e3a9b38b7a2071559c482066fa
Rico Buchholz <buchholz@tugraz.at>
2018-11-26 12:56:51 +0100

    Adjustements to allow QL to use the testscript.
    Introduce third command lines parameter. This depicts the type of the
    code (PAR or QL).
    Introduce variable for the start and end value of the loop for
    switch_reconstruct. The end will be set depending on the code.

--- ShellScripts/test_full_reconstruct.sh
+++ ShellScripts/test_full_reconstruct.sh
@@ -5,6 +5,7 @@
 
 testcase=${1}
 number_processors=${2}
+which_code=${3}
 
 testpath=`mktemp -d /temp/$LOGNAME/Neo2/Testing/Runs/Test-XXXXXX`
 echo "Testpath created: ${testpath}"
@@ -17,6 +18,9 @@ executablename="neo_2.x"
 totalnumberofstages=4
 numberofstage=0
 return_value=0
+
+START=0
+END=3
 
 check_absolute_if_relative_fails=yes
 
@@ -98,7 +102,7 @@ cp -r ../../Template/${testcase}/ .
 cd ${testcase}
 echo "Running Test ${testcase}..."
 
-if [ ${number_processors} -eq 0 ]; then
+if [ ${number_processors} -eq 0 ] ; then
   echo "Sequential mode"
   runcommand="$testpath/$executablename"
 else
@@ -106,7 +110,12 @@ else
   runcommand="mpirun -np ${number_processors} $testpath/$executablename"
 fi
 
-for numberofstage in 0 1 2 3 ; do
+if [ "x${which_code}" = "xQL" ] ; then
+  END=0
+fi
+
+numberofstage=$START
+while [ $numberofstage -le $END ] ; do
   switch_reconstruction.sh $numberofstage
   $runcommand >> job.log 2>&1
   # check_equality_dat is a function.
@@ -116,6 +125,7 @@ for numberofstage in 0 1 2 3 ; do
     echo "Test ($numberofstage/$totalnumberofstages) failed. Not all files are equal."
     return_value=1
   fi
+  numberofstage=$(($numberofstage+1))
 done
 
 if check_equality_hdf5 $referencepath ${testcase} ; then
