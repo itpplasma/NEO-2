dee30c9d2ba2ed8b0f21d5671bfa9b02a7697b45
Rico Buchholz <buchholz@tugraz.at>
2018-12-18 10:43:48 +0100

    Add function to resize the arrays of a hdf5 file.
    Added a function to hdf5tools, that allows to resize (some) arrays in a
    hdf5 file.
    Main application in mind was resizing a radial hdf5 input file for a
    scan, e.g. to exclude the axis from a radial scan, outer points where
    the temperature get negative, or points where it is known, that there is
    no drive by a mode.

--- PythonScripts/hdf5tools.py
+++ PythonScripts/hdf5tools.py
@@ -195,6 +195,252 @@ def reshape_hdf5_file(in_filename: str, out_filename: str):
 
     i = i + 1
 
+def dim_zero(data, lower_bounds, upper_bounds):
+  return data
+
+def dim_one(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0]]
+
+def dim_two(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1]]
+
+def dim_three(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2]]
+
+def dim_four(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3]]
+
+def dim_five(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4]]
+
+def dim_six(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5]]
+
+def dim_seven(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5],
+              lower_bounds[6]:upper_bounds[6]]
+
+def dim_eigth(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5],
+              lower_bounds[6]:upper_bounds[6],
+              lower_bounds[7]:upper_bounds[7]]
+
+def dim_nine(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5],
+              lower_bounds[6]:upper_bounds[6],
+              lower_bounds[7]:upper_bounds[7],
+              lower_bounds[8]:upper_bounds[8]]
+
+def dim_ten(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5],
+              lower_bounds[6]:upper_bounds[6],
+              lower_bounds[7]:upper_bounds[7],
+              lower_bounds[8]:upper_bounds[8],
+              lower_bounds[9]:upper_bounds[9]]
+
+def dim_eleven(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5],
+              lower_bounds[6]:upper_bounds[6],
+              lower_bounds[7]:upper_bounds[7],
+              lower_bounds[8]:upper_bounds[8],
+              lower_bounds[9]:upper_bounds[9],
+              lower_bounds[10]:upper_bounds[10]]
+
+def dim_twelve(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5],
+              lower_bounds[6]:upper_bounds[6],
+              lower_bounds[7]:upper_bounds[7],
+              lower_bounds[8]:upper_bounds[8],
+              lower_bounds[9]:upper_bounds[9],
+              lower_bounds[10]:upper_bounds[10],
+              lower_bounds[11]:upper_bounds[11]]
+
+def dim_thirteen(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5],
+              lower_bounds[6]:upper_bounds[6],
+              lower_bounds[7]:upper_bounds[7],
+              lower_bounds[8]:upper_bounds[8],
+              lower_bounds[9]:upper_bounds[9],
+              lower_bounds[10]:upper_bounds[10],
+              lower_bounds[11]:upper_bounds[11],
+              lower_bounds[12]:upper_bounds[12]]
+
+def dim_fourteen(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5],
+              lower_bounds[6]:upper_bounds[6],
+              lower_bounds[7]:upper_bounds[7],
+              lower_bounds[8]:upper_bounds[8],
+              lower_bounds[9]:upper_bounds[9],
+              lower_bounds[10]:upper_bounds[10],
+              lower_bounds[11]:upper_bounds[11],
+              lower_bounds[12]:upper_bounds[12],
+              lower_bounds[13]:upper_bounds[13]]
+
+def dim_fiveteen(data, lower_bounds, upper_bounds):
+  return data[lower_bounds[0]:upper_bounds[0],
+              lower_bounds[1]:upper_bounds[1],
+              lower_bounds[2]:upper_bounds[2],
+              lower_bounds[3]:upper_bounds[3],
+              lower_bounds[4]:upper_bounds[4],
+              lower_bounds[5]:upper_bounds[5],
+              lower_bounds[6]:upper_bounds[6],
+              lower_bounds[7]:upper_bounds[7],
+              lower_bounds[8]:upper_bounds[8],
+              lower_bounds[9]:upper_bounds[9],
+              lower_bounds[10]:upper_bounds[10],
+              lower_bounds[11]:upper_bounds[11],
+              lower_bounds[12]:upper_bounds[12],
+              lower_bounds[13]:upper_bounds[13],
+              lower_bounds[14]:upper_bounds[14]]
+
+def resize_hdf5_file(in_filename: str, out_filename: str, original_size: int, first: int, last: int, operate_on_last_dimension: bool):
+  """Resize the arrays of an hdf5 file.
+
+  input
+  ----------
+  in_filename: File to read in and which content should be resized.
+  out_filename: Name under which to store the resized file.
+  original_size: The size of the (dimension of the) arrays which to
+    resize. This is also used to determine if an array should be
+    changed. For example if original_size=100 then 2x2 arrays (e.g.
+    containing species data) will be left untouched.
+  first: the zero based index of the first array item to keep.
+  last: the zero based index of the array item up to which to keep. This
+    index is not included, i.e. if given last=10 then only array items
+    up to index 9 will be kept.
+  operate_on_last_dimension: If this is true, then for multidimensional
+    arrays, the last dimension will be resized. If false, then this
+    function operates on the first dimension.
+
+  return value
+  ----------
+  None, output is realized via side effect.
+
+  side effects
+  ----------
+  Creates hdf5-file with given name 'out_filename', if first < last.
+
+  limitations
+  ----------
+  So far it is assumed that the key with the number of points for the
+  resized dimension is '/num_radial_pts'.
+  """
+
+  import h5py
+  import numpy as np
+
+  # If the 'list' of indices is empty, do nothing.
+  if first >= last:
+    return
+
+  f = get_hdf5file(in_filename)
+  o = get_hdf5file_new(out_filename)
+
+  for key in list(f.keys()):
+    size = list(f[key].shape)
+
+    lower_bounds = [0 for x in range(len(size))]
+    upper_bounds = [x for x in size]
+
+    if operate_on_last_dimension:
+      index_to_change = len(size)-1
+    else:
+      index_to_change = 0
+
+    if (original_size == size[index_to_change]):
+      size[index_to_change] = last - first
+      lower_bounds[index_to_change] = first
+      upper_bounds[index_to_change] = last
+
+    #~ dat = o.create_dataset(key, shape=tuple(size), dtype=f[key].dtype)
+
+    # Fill the dataset with values. This codes uses a suggestion from
+    #https://stackoverflow.com/questions/11479816/what-is-the-python-equivalent-for-a-case-switch-statement/11479840#11479840
+    # to recreate a switch/select statement in python.
+
+    # map the inputs to the function blocks
+    options = {0 : dim_zero,
+               1 : dim_one,
+               2 : dim_two,
+               3 : dim_three,
+               4 : dim_four,
+               5 : dim_five,
+               6 : dim_six,
+               7 : dim_seven,
+               8 : dim_eigth,
+               9 : dim_nine,
+               10: dim_ten,
+               11: dim_eleven,
+               12: dim_twelve,
+               13: dim_thirteen,
+               14: dim_fourteen,
+               15: dim_fiveteen,
+    }
+
+    # Call the corresponding function, which corresponds to the body of
+    # the 'case'.
+    #~ dat = options[len(size)](f[key], lower_bounds, upper_bounds)
+    dat = options[len(size)](f[key], lower_bounds, upper_bounds)
+
+    o.create_dataset(key, data=dat)
+
+  o['/num_radial_pts'][()] = np.array([last - first])
+
 if __name__ == "__main__":
 
   import h5py
