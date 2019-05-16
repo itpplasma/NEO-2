522aa0503f052d2c5448838f1220c91b12f9a6cb
Rico Buchholz <buchholz@tugraz.at>
2019-05-16 14:18:57 +0200

    Add function to change read/write switch and vphi
    Add a function, to change the switch lsw_read_precom and
    lsw_write_precom to true/false, respectively, and to set vphi, for
    neo2.in files in subfolders.

--- PythonScripts/scan_nonhdf5_tools.py
+++ PythonScripts/scan_nonhdf5_tools.py
@@ -79,6 +79,88 @@ def get_memory_consumption_of_scan(lines, id_scan: str, num_runs: int):
 
   return memory_consumption
 
+def set_neo2in(folder: str, subfolder_pattern: str, vphifilename: str, backup: bool):
+  """Set read/write switches and vphi in neo2in files in subfolders.
+
+  This function is intended to set the switches lsw_read_precom and
+  lsw_write_precom for a computation run, i.e. the former is set to true
+  and the latter to false.
+  Also, vphi will be set, the value will be read from the file
+  vphifilename, and if this is not found, a default is used. The file
+  must contain the value in the form 'vphiref = 0' in the first line.
+
+  Example usage:
+  from scan_nonhdf5_tools import set_neo2in
+  set_neo2in('./', 'es_*', 'vphiref.in', True)
+
+  input:
+  ------
+  folder: string with the folder where the subfolders are located. If
+    you are in the same directory, then './' can be used.
+  subfolder_pattern: string which describes the subfolders. This may
+    contain wildcards, e.g. 'es_*'.
+    Attention: the pattern should not include files that are also
+    present in the directory, as there is no check, to operate on
+    folders only.
+  vphifilename: string, with the name of the file, that contains the
+    value for vphi. If the file can not be found, this is not considered
+    an error, instead a default value is used.
+  backup: boolean, if true, a backup of the original input file is
+    stored with added '~' at the end.
+  """
+  from os.path import join
+  from pathlib import Path
+
+  import f90nml.parser
+
+  # Check if vphiref.in is present.
+  #   if so, extract the value of vphi.
+  # create list of folders/iterate over folders.
+  # for each folder
+  #   read in neo2.in
+  #   eventually backup neo2.in
+  #   set lsw_read_precom to true and lsw_write_precom to false.
+  #   set vphi
+  #   write neo2.in
+
+  vphi = 0.0
+  try:
+    with open(join(folder, vphifilename)) as f:
+      line = f.readline()
+      parts = line.split('=')
+      if (len(parts) > 1):
+        vphi = float(parts[1].split('!')[0])
+  except FileNotFoundError:
+    # If the file is not found, this is not considered to be an error,
+    # just leave vphi on the default value.
+    print('\n')
+    print('File "' + vphifilename + '" not found.')
+    print('Using default value: {:e}'.format(vphi))
+    print('\n')
+
+  # Get all objects in the folder
+  p = Path(folder)
+  #~ # Filter for the directories
+  #~ p = [x for x in p.iterdir() if x.is_dir()]
+  # Filter for the name of the subdirectories
+  folders = list(p.glob(subfolder_pattern))
+
+  parser = f90nml.parser.Parser()
+  for d in folders:
+    current_filename = join(folder, d.name, 'neo2.in')
+    nml = parser.read(current_filename)
+
+    print('Processing: ' + current_filename)
+
+    if (backup):
+      nml.write(current_filename + '~')
+
+    nml['collision']['lsw_read_precom'] = True
+    nml['collision']['lsw_write_precom'] = False
+
+    nml['multi_spec']['vphi'] = vphi
+    nml.write(current_filename, True)
+
 if __name__ == "__main__":
   import matplotlib.pyplot as plt
   import sys
