6539c7c3d25785824f52b7fbfdb4e9bb1f844c91
Rico Buchholz <buchholz@tugraz.at>
2019-05-21 11:24:09 +0200

    Change behaviour of function: replace file.
    The function copy_hdf5_from_subfolders_to_single_file will now replace
    'outfilename' if it already exists.

--- PythonScripts/hdf5tools.py
+++ PythonScripts/hdf5tools.py
@@ -114,7 +114,7 @@ def copy_hdf5_from_subfolders_to_single_file(path: str, infilename: str, outfile
   folders = [join(path, f) for f in listdir(path) if not isfile(join(path, f))]
 
   try:
-    o = get_hdf5file_new(outfilename)
+    o = get_hdf5file_replace(join(path, outfilename))
   except OSError as error:
     print('OSError: {0}'.format(error))
     raise error
