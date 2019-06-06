4e85df56a0e5380d8af526ebbbc87cb84f0bc8bf
Rico Buchholz <buchholz@tugraz.at>
2019-06-06 18:04:11 +0200

    Change in unused code.
    Make simplification of the code (which would not be executed at the
    moment). This should change the behaviour and the output.
    Code remains 'behind the end' as it is not sure if it works as intended.

--- ShellScripts/set_neo2in.sh
+++ ShellScripts/set_neo2in.sh
@@ -19,16 +19,16 @@ fname_neo2in="neo2.in"
 # Option -d: list only directories (not their content).
 dirs="$(ls -d es_*)"
 
-for folder in ${dirs}; do
-  filename="$folder/neo2.in"
-  echo "filename: $filename"
-  cp $filename $filename.sav
+#~ for folder in ${dirs}; do
+  #~ filename="$folder/neo2.in"
+  #~ echo "filename: $filename"
+  #~ cp $filename $filename.sav
 
-  sed -i "s/.*lsw_read_precom.*/ LSW_READ_PRECOM=T/" $filename
-  sed -i "s/.*LSW_READ_PRECOM.*/ LSW_READ_PRECOM=T/" $filename
-  sed -i "s/.*lsw_write_precom.*/ LSW_WRITE_PRECOM=F/" $filename
-  sed -i "s/.*LSW_WRITE_PRECOM.*/ LSW_WRITE_PRECOM=F/" $filename
-done
+  #~ sed -i "s/.*lsw_read_precom.*/ LSW_READ_PRECOM=T/" $filename
+  #~ sed -i "s/.*LSW_READ_PRECOM.*/ LSW_READ_PRECOM=T/" $filename
+  #~ sed -i "s/.*lsw_write_precom.*/ LSW_WRITE_PRECOM=F/" $filename
+  #~ sed -i "s/.*LSW_WRITE_PRECOM.*/ LSW_WRITE_PRECOM=F/" $filename
+#~ done
 
 ########################################################################
 
@@ -50,50 +50,21 @@ fi
 for dir in ${dirs}; do
   cd "${dir}"
   echo "Processing ${fname_neo2in} in ${dir}/ ..."
-  if [ -e "${fname_neo2in}.sav" ]; then
-    if [ -e ${fname_neo2in} ]; then
-      vphi=$(cat "${fname_neo2in}.sav" | awk '/^ VPHI=/ {gsub(/[^0-9.\-]/,""); print}')
+  if [ -e "${fname_neo2in}" ]; then
+    cp ${fname_neo2in} ${fname_neo2in}.sav
 
-      vphi_new=$(echo -e "${vphi} + ${vphishift}" | bc)
+    vphi=$(cat "${fname_neo2in}.sav" | awk '/^ VPHI=/ {gsub(/[^0-9.\-]/,""); print}')
 
-      cat ${fname_neo2in} | awk '{\
-      gsub(/^ VPHI=[ 0-9.\-]+/," VPHI='${vphi_new}'"); \
-      print}' > ${fname_neo2in}
-    fi
+    vphi_new=$(echo -e "${vphi} + ${vphishift}" | bc)
+
+    cat "${fname_neo2in}.sav" | awk '{\
+    gsub(/^ VPHI=[ 0-9.\-]+/," VPHI='${vphi_new}'"); \
+    sub("LSW_READ_PRECOM=F","LSW_READ_PRECOM=T"); \
+    sub("LSW_WRITE_PRECOM=T","LSW_WRITE_PRECOM=F"); \
+    print}' > ${fname_neo2in}
+
   else
-    echo "${fname_neo2in}.sav does not exist. Save existing neo2.in as neo2.in.err!"
-    echo "This directory will be excluded from Condor runs! Check input files and run NEO-2 manually!"
-    cat ${fname_neo2in} > "${fname_neo2in}.err"
-  fi
-  sync
-  sleep 0.1
-  sync
-  cd ..
-done
-
-########################################################################
-
-for dir in ${dirs}; do
-  cd "${dir}"
-  echo "Processing ${fname_neo2in} in ${dir}/ ..."
-  if [ -e "${fname_neo2in}.sav" ]; then
-    if [ ! -e neo2_multispecies_out.h5 ]; then
-      echo -e "Output file neo2_multispecies_out.h5 not found in directory ${dir}"
-
-      vphi=$(cat "${fname_neo2in}.sav" | awk '/^ VPHI=/ {gsub(/[^0-9.\-]/,""); print}')
-
-      vphi_new=$(echo -e "${vphi} + ${vphishift}" | bc)
-
-      cat "${fname_neo2in}.sav" | awk '{\
-      gsub(/^ VPHI=[ 0-9.\-]+/," VPHI='${vphi_new}'"); \
-      sub("LSW_READ_PRECOM=F","LSW_READ_PRECOM=T"); \
-      sub("LSW_WRITE_PRECOM=T","LSW_WRITE_PRECOM=F"); \
-      print}' > ${fname_neo2in}
-    fi
-  else
-    echo "${fname_neo2in}.sav does not exist. Save existing neo2.in as neo2.in.err!"
-    echo "This directory will be excluded from Condor runs! Check input files and run NEO-2 manually!"
-    cat ${fname_neo2in} > "${fname_neo2in}.err"
+    echo "ERROR: ${fname_neo2in} does not exist."
   fi
   sync
   sleep 0.1
