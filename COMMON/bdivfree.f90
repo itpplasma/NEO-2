41c75032dbf80fcf920caf50ed36ec9f90c3790b
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 10:10:49 +0200

    One module per file: theta_rz_mod.
    Move the module theta_rz_mod to an own file.

--- COMMON/bdivfree.f90
+++ COMMON/bdivfree.f90
@@ -8,18 +8,6 @@
     double precision, dimension(:,:,:,:), allocatable :: aznre,aznim,arnre,arnim
   end module bdivfree_mod
 !
-!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-!
-  module theta_rz_mod
-    integer :: icall=0
-    integer :: nsqp,nlab,nthe,icp_pt
-    integer, dimension(:,:), allocatable :: ipoint_pt
-    real(kind=8) :: hsqpsi,hlabel,htheqt,psiaxis,sigma_qt,raxis,zaxis
-    real(kind=8), dimension(:,:),   allocatable :: spllabel
-    real(kind=8), dimension(:,:,:), allocatable :: splthet
-    real(kind=8), dimension(:),     allocatable :: sqpsi,flab,theqt
-  end module theta_rz_mod
-!
 !ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 !
   module amn_mod
