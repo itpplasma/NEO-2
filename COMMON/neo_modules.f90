0becc5a98ef10ab5a0dc2eb5ea3ad78e8bc7762e
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 15:40:28 +0200

    One module per file: neo_spline_b00.
    Move module neo_spline_b00 to own file.

--- COMMON/neo_modules.f90
+++ COMMON/neo_modules.f90
@@ -1,10 +1,3 @@
-MODULE neo_spline_b00
-  ! spline for b00
-  USE neo_precision
-  REAL(kind=dp), DIMENSION(:),   ALLOCATABLE :: a_b00, b_b00, c_b00, d_b00
-  
-END MODULE neo_spline_b00
-
 MODULE neo_work
 ! Working parameters
   USE neo_precision
