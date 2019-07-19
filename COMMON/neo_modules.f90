fcc66eb9824a287a505eb2243bfa1183ac1c1451
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 14:07:51 +0200

    One module per file: neo_van.
    Move module neo_van into an own file.

--- COMMON/neo_modules.f90
+++ COMMON/neo_modules.f90
@@ -368,23 +368,6 @@ MODULE partpa_pla
   REAL(kind=dp), DIMENSION(:), ALLOCATABLE :: p_fac
 END MODULE partpa_pla
 
-MODULE neo_van
-  USE neo_precision
-  REAL(kind=dp)                            :: v_phi0, v_theta0
-  REAL(kind=dp)                            :: bmin_tol
-  INTEGER                                  :: v_nper, v_steps
-  INTEGER                                  :: v_num_mm
-  INTEGER                                  :: no_minima
-  INTEGER,  DIMENSION(:), ALLOCATABLE      :: li_minima
-  INTEGER                                  :: no_gamma
-  INTEGER                                  :: tau_num
-  INTEGER                                  :: tau_max_iter
-  REAL(kind=dp)                            :: lambda_fac
-  REAL(kind=dp)                            :: temp_e
-  REAL(kind=dp)                            :: gamma_eps
-  REAL(kind=dp)                            :: phi_eps
-END MODULE neo_van
-
 MODULE neo_van_exchange
   USE neo_precision
   REAL(kind=dp)                            :: rho_fac, jperp_fac
