2d90c11d5ce9216c910df179b43739ec259a15cb
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 17:03:23 +0200

    One module per file: sizey_pla.
    Move module sizey_pla to own file.

--- COMMON/neo_modules.f90
+++ COMMON/neo_modules.f90
@@ -42,17 +42,6 @@ MODULE partpa_cur
   INTEGER                                  :: write_curint
 END MODULE partpa_cur
 
-MODULE sizey_pla
-  USE neo_precision
-  ! Definition for rk4d_pla 
-  INTEGER            ::  npart_pla
-  INTEGER            ::  ndim_pla
-  INTEGER, PARAMETER ::  npq_pla = 3
-  REAL(kind=dp)      ::  lamup_pla
-  REAL(kind=dp)      ::  lambda_alpha
-  REAL(kind=dp)      ::  nufac_pla  
-END MODULE sizey_pla
-
 MODULE partpa_pla
   USE neo_precision
   ! Exchange between flint_pla and rhs_pla
