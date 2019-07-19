ddaa3327dd36e2c93c69fc8728fd2bcd9ced1ac7
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 15:03:56 +0200

    One module per file: neo_actual_fluxs.
    Move module neo_actual_fluxs to own file.

--- COMMON/neo_modules.f90
+++ COMMON/neo_modules.f90
@@ -1,15 +1,3 @@
-MODULE neo_actual_fluxs
-! Actual data on flux surface
-  USE neo_precision
-  REAL(kind=dp)                                   :: s_es
-  REAL(kind=dp)                                   :: s_iota
-  REAL(kind=dp)                                   :: s_pprime
-  REAL(kind=dp)                                   :: s_sqrtg00
-  REAL(kind=dp)                                   :: s_curr_tor, s_curr_pol
-  REAL(kind=dp)                                   :: s_b00, s_b00_s
-  INTEGER                                         :: through_fourier
-END MODULE neo_actual_fluxs
-
 MODULE neo_actual_spectra
 ! Actual spectra on flux surface
   USE neo_precision
