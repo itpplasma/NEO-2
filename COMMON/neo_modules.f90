5d945c05624715933a7b9bf1c1b3aa95031a8505
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 16:31:55 +0200

    One module per file: neo_control.
    Move module neo_control to own file.

--- COMMON/neo_modules.f90
+++ COMMON/neo_modules.f90
@@ -17,29 +17,6 @@ MODULE neo_output
   REAL(kind=dp)                            ::  delta_cur_max, typ_cur_len
 END MODULE neo_output
 
-MODULE neo_control
-! Control parameters from input file
-  USE neo_precision
-  CHARACTER(20)                      :: in_file
-  INTEGER                            :: theta_n
-  INTEGER                            :: phi_n
-  INTEGER                            :: s_ind_in
-  INTEGER                            :: write_progress
-  INTEGER                            :: write_output_files
-  INTEGER                            :: calc_fourier
-  INTEGER                            :: spline_test
-  INTEGER                            :: max_m_mode, max_n_mode
-  INTEGER                            :: lab_swi, inp_swi, ref_swi, eout_swi
-  INTEGER                            :: chk_swi
-  INTEGER                            :: fluxs_interp
-  INTEGER                            :: s_num
-  REAL(kind=dp)                      :: s_start, s_end
-  INTEGER                            :: g11_swi
-  INTEGER                            :: eval_mode
-  INTEGER                            :: no_fluxs, no_fluxs_s
-  INTEGER, DIMENSION(:), ALLOCATABLE :: fluxs_arr
-END MODULE neo_control
-
 MODULE neo_units
 ! Units and Formats
   USE neo_precision
