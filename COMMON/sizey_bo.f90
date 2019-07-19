e10cef2c33fd9fc4a264a70d3ad4fc3f862bf781
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 16:53:48 +0200

    One module per file: sizey_bo.
    Move module sizey_bo to own file.

---
+++ COMMON/sizey_bo.f90
@@ -0,0 +1,8 @@
+MODULE sizey_bo
+  USE neo_precision
+! Definition for rk4d_bo also used in main routine neo
+  INTEGER            ::  npart
+  INTEGER            ::  multra
+  INTEGER            ::  ndim
+  INTEGER, PARAMETER ::  npq = 7
+END MODULE sizey_bo
