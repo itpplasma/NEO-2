706aa4267fbbdb9696f909204c912710bc6126d7
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 13:04:24 +0200

    One module per file: field_eq_mod.
    Move module field_eq_mod to own file.

--- COMMON/field_divB0.f90
+++ COMMON/field_divB0.f90
@@ -2,19 +2,6 @@ module period_mod
   double precision :: per_phi, per_tht
 end module period_mod
 
-module field_eq_mod
-  integer :: icall_eq=0
-  integer :: nrad,nzet,icp,nwindow_r,nwindow_z
-  real(kind=8), parameter                      :: pi=3.14159265358979d0
-  real(kind=8) :: psib,btf,rtf,hrad,hzet
-  real(kind=8), dimension(:,:), allocatable    :: psi, psi0
-  real(kind=8), dimension(:,:,:), allocatable  :: splpsi
-  real(kind=8), dimension(:), allocatable      :: rad, zet, xi,f
-  integer, dimension(:), allocatable           :: imi,ima,jmi,jma
-  integer, dimension(:,:), allocatable         :: ipoint
-  double precision :: psif,dpsidr,dpsidz,d2psidr2,d2psidrdz,d2psidz2
-end module field_eq_mod
-!
 module field_mod
   integer          :: icall=0
   integer          :: ipert,iequil
