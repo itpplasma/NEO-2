9ce03854e078c9f344983bc90a872374533607b3
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 16:18:14 +0200

    One module per file: neo_spline.
    Move module neo_spline to own file.

--- COMMON/neo_modules.f90
+++ COMMON/neo_modules.f90
@@ -17,30 +17,6 @@ MODULE neo_output
   REAL(kind=dp)                            ::  delta_cur_max, typ_cur_len
 END MODULE neo_output
 
-MODULE neo_spline
-! Spline arrays
-  USE neo_precision
-  INTEGER, PARAMETER       ::   mt = 1
-  INTEGER, PARAMETER       ::   mp = 1
-  INTEGER                  ::   theta_ind, phi_ind
-  INTEGER                  ::   ierr
-
-  REAL(kind=dp) ::  theta_d, phi_d 
-
-! Spline array for modb
-  REAL(kind=dp),    DIMENSION(:,:,:,:), ALLOCATABLE :: b_spl 
-! Spline array for geodesic curviture
-  REAL(kind=dp),    DIMENSION(:,:,:,:), ALLOCATABLE :: k_spl
-! Spline array for sqrg11
-  REAL(kind=dp),    DIMENSION(:,:,:,:), ALLOCATABLE :: g_spl
-! Spline array for parallel derivative
-  REAL(kind=dp),    DIMENSION(:,:,:,:), ALLOCATABLE :: p_spl
-! Spline array for quasi-toroidal phi component of b
-  REAL(kind=dp),    DIMENSION(:,:,:,:), ALLOCATABLE :: q_spl
-! Spline array for r_nabpsi
-  REAL(kind=dp),    DIMENSION(:,:,:,:), ALLOCATABLE :: r_spl
-END MODULE neo_spline
-
 MODULE neo_control
 ! Control parameters from input file
   USE neo_precision
