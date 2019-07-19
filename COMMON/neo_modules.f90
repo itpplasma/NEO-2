309fa55af95c1d14436a6972e15ab2d2fd482bb3
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 15:19:37 +0200

    One module per file: neo_actual_spectra.
    Move module neo_actual_spectra into own file.

--- COMMON/neo_modules.f90
+++ COMMON/neo_modules.f90
@@ -1,15 +1,3 @@
-MODULE neo_actual_spectra
-! Actual spectra on flux surface
-  USE neo_precision
-  REAL(kind=dp),    DIMENSION(:),     ALLOCATABLE :: s_rmnc, s_zmnc, s_lmnc
-  REAL(kind=dp),    DIMENSION(:),     ALLOCATABLE :: s_bmnc
-  !! Modifications by Andreas F. Martitsch (06.08.2014)
-  ! Additional data from Boozer files without Stellarator symmetry
-  REAL(kind=dp),    DIMENSION(:),     ALLOCATABLE :: s_rmns, s_zmns, s_lmns
-  REAL(kind=dp),    DIMENSION(:),     ALLOCATABLE :: s_bmns
-  !! End Modifications by Andreas F. Martitsch (06.08.2014)
-END MODULE neo_actual_spectra
-
 MODULE neo_spline_data
   ! Splines along s
   USE neo_precision
