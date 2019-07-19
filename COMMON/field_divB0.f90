bc880c94a5aae3881fe1f129e4ad6177bddde377
Rico Buchholz <buchholz@tugraz.at>
2019-07-19 13:26:46 +0200

    One module per file: inthecore_mod.
    Move module inthecore_mod to an own file.

--- COMMON/field_divB0.f90
+++ COMMON/field_divB0.f90
@@ -8,17 +8,6 @@ module field_mod
   double precision :: ampl
 end module field_mod
 !
-module inthecore_mod
-  logical :: prop=.true.
-  integer :: npoi,ijumpb,ibeg,iend
-  double precision, parameter :: epssep=1.d-6
-  double precision :: rc,zc,twopi,sig,psi_sep,psi_cut,sigpsi,cutoff
-  double precision, dimension(:), allocatable :: rho2i,theti
-  integer          :: incore
-  double precision :: vacf,dvacdr,dvacdz,d2vacdr2,d2vacdrdz,d2vacdz2
-  double precision :: plaf,dpladr,dpladz,d2pladr2,d2pladrdz,d2pladz2
-end module inthecore_mod
-!
 !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 ! -----------------------------------------------------------------
 subroutine field(r,p,z,Br,Bp,Bz,dBrdR,dBrdp,dBrdZ   &
