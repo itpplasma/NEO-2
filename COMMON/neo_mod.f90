3e4778730ab394e1681f42df8d1f190cee691fb5
Rico Buchholz <buchholz@tugraz.at>
2019-07-22 13:26:43 +0200

    One module per file: rkstep_mod.
    Move module rkstep_mod to own file.

--- COMMON/neo_mod.f90
+++ COMMON/neo_mod.f90
@@ -12,46 +12,6 @@
 !!$    DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: amat_minus_minus
 !!$  END MODULE ripple_solver_mod
 !
-  MODULE rkstep_mod
-    INTEGER :: legmax,leg,lag
-    !**********************************************************
-    ! Changes required for multispecies support
-    !**********************************************************
-    ! Old Version
-    ! DOUBLE PRECISION, DIMENSION(:,:),     ALLOCATABLE :: anumm,denmm,asource
-    ! DOUBLE PRECISION, DIMENSION(:,:,:),   ALLOCATABLE :: ailmm
-    ! DOUBLE PRECISION, DIMENSION(:,:),     ALLOCATABLE :: weightlag
-    
-    ! New Version - up to now only differential part
-    CHARACTER(len=3), DIMENSION(:), ALLOCATABLE       :: species_tags
-    DOUBLE PRECISION, DIMENSION(:,:),     ALLOCATABLE :: Amm
-    DOUBLE PRECISION, DIMENSION(:,:),     ALLOCATABLE :: asource
-    DOUBLE PRECISION, DIMENSION(:,:,:),   ALLOCATABLE, TARGET :: anumm_a
-    DOUBLE PRECISION, DIMENSION(:,:,:),   ALLOCATABLE, TARGET :: denmm_a
-    DOUBLE PRECISION, DIMENSION(:,:,:,:), ALLOCATABLE, TARGET :: anumm_aa
-    DOUBLE PRECISION, DIMENSION(:,:,:,:), ALLOCATABLE, TARGET :: denmm_aa
-    DOUBLE PRECISION, DIMENSION(:,:,:,:,:),ALLOCATABLE, TARGET:: ailmm_aa    
-    DOUBLE PRECISION, DIMENSION(:,:),     POINTER     :: anumm
-    DOUBLE PRECISION, DIMENSION(:,:),     ALLOCATABLE :: anumm_lag
-    DOUBLE PRECISION, DIMENSION(:,:),     POINTER     :: denmm
-    DOUBLE PRECISION, DIMENSION(:,:,:),   POINTER     :: ailmm
-    DOUBLE PRECISION, DIMENSION(:,:),     ALLOCATABLE :: weightlag
-    DOUBLE PRECISION, DIMENSION(:),       ALLOCATABLE :: weightden
-    DOUBLE PRECISION, DIMENSION(:),       ALLOCATABLE :: weightparflow
-    DOUBLE PRECISION, DIMENSION(:),       ALLOCATABLE :: weightenerg
-    double precision, dimension(:,:,:),   allocatable :: Inbi_lmmp_a
-    !**********************************************************
-    ! WINNY - for flint
-    ! DOUBLE PRECISION, DIMENSION(:),       ALLOCATABLE :: collision_sigma_multiplier
-    ! WINNY - for flint
-    !**********************************************************
-    DOUBLE PRECISION               :: epserr_sink       ! Regularization
-    DOUBLE COMPLEX                 :: epserr_sink_cmplx ! Regularization
-    DOUBLE PRECISION               :: epserr_iter
-    INTEGER                        :: niter
-    DOUBLE PRECISION, DIMENSION(3) :: fluxes
-  END MODULE
-!
 MODULE development
 INTEGER :: iprintflag,proptag_old=-100000000,nstfp
 DOUBLE PRECISION :: deleta
