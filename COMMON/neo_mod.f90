52050a2b8272d2daf21a94a210043d67da01912a
Rico Buchholz <buchholz@tugraz.at>
2019-07-22 14:58:39 +0200

    One module per file: development.
    Move module development to own file.

--- COMMON/neo_mod.f90
+++ COMMON/neo_mod.f90
@@ -11,23 +11,6 @@
 !!$    DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: amat_plus_plus
 !!$    DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: amat_minus_minus
 !!$  END MODULE ripple_solver_mod
-!
-MODULE development
-INTEGER :: iprintflag,proptag_old=-100000000,nstfp
-DOUBLE PRECISION :: deleta
-DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE :: alam_l,alam_r
-DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE :: delta_eta_l,delta_eta_r
-DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE :: phi_stfp
-DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: stfp_ov_hstep
-INTEGER :: solver_talk
-INTEGER :: switch_off_asymp
-! accuracy factor in ripple solver
-DOUBLE PRECISION :: ripple_solver_accurfac
-! asymptotical stuff
-INTEGER :: asymp_margin_zero
-INTEGER :: asymp_margin_npass
-DOUBLE PRECISION :: asymp_pardeleta
-END MODULE
 
 ! Winny module for ripple added
 MODULE ripple_mod
