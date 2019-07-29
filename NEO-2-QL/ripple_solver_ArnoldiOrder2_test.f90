38770d9d8eb2422a3b935158aecda44e7b8824c6
Rico Buchholz <buchholz@tugraz.at>
2019-07-29 13:37:40 +0200

    Fix intendation in subroutine.

--- NEO-2-QL/ripple_solver_ArnoldiOrder2_test.f90
+++ NEO-2-QL/ripple_solver_ArnoldiOrder2_test.f90
@@ -5829,38 +5829,38 @@ CALL mpro%allgather(scalprod_pleg(:,:,:,ispec), scalprod_pleg)
 !>                  ritznum  - eigenvalues
 !>                  eigh     - eigenvectors
 !>                  ierr     - error code (0 - normal work)
-  SUBROUTINE try_eigvecvals(m,tol,hmat,ngrow,ritznum,eigh,ierr)
+  subroutine try_eigvecvals(m,tol,hmat,ngrow,ritznum,eigh,ierr)
 
-  IMPLICIT NONE
-!
-  INTEGER :: m,ngrow,ierr,k,j,lwork,info
+    implicit none
+
+    integer :: m,ngrow,ierr,k,j,lwork,info
 
     double precision, parameter :: tiny_diff = 1.d-12
 
-  DOUBLE PRECISION :: tol
-  DOUBLE COMPLEX   :: tmp
-!
-  DOUBLE COMPLEX, DIMENSION(m)   :: ritznum
-  !! Modification by Andreas F. Martitsch (19.10.2016)
-  ! old:
-  !DOUBLE COMPLEX, DIMENSION(m,m) :: hmat,eigh
-  ! new:
-  DOUBLE COMPLEX, DIMENSION(:,:) :: hmat
-  DOUBLE COMPLEX, DIMENSION(m,m) :: eigh
-  !! End Modification by Andreas F. Martitsch (19.10.2016)
-!
-  LOGICAL,          DIMENSION(:),   ALLOCATABLE :: selec
-  INTEGER,          DIMENSION(:),   ALLOCATABLE :: ifailr
-  DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE :: rwork
-  DOUBLE COMPLEX,   DIMENSION(:),   ALLOCATABLE :: work,rnum
-  DOUBLE COMPLEX,   DIMENSION(:,:), ALLOCATABLE :: hmat_work
-!
-  ierr=0
-!
-  ALLOCATE(hmat_work(m,m))
-!
-  hmat_work=hmat
-!
+    double precision :: tol
+    double complex   :: tmp
+
+    double complex, dimension(m)   :: ritznum
+    !! Modification by Andreas F. Martitsch (19.10.2016)
+    ! old:
+    !DOUBLE COMPLEX, DIMENSION(m,m) :: hmat,eigh
+    ! new:
+    DOUBLE COMPLEX, DIMENSION(:,:) :: hmat
+    DOUBLE COMPLEX, DIMENSION(m,m) :: eigh
+    !! End Modification by Andreas F. Martitsch (19.10.2016)
+
+    LOGICAL,          DIMENSION(:),   ALLOCATABLE :: selec
+    INTEGER,          DIMENSION(:),   ALLOCATABLE :: ifailr
+    DOUBLE PRECISION, DIMENSION(:),   ALLOCATABLE :: rwork
+    DOUBLE COMPLEX,   DIMENSION(:),   ALLOCATABLE :: work,rnum
+    DOUBLE COMPLEX,   DIMENSION(:,:), ALLOCATABLE :: hmat_work
+
+    ierr=0
+
+    ALLOCATE(hmat_work(m,m))
+
+    hmat_work=hmat
+
 !!$  WRITE(*,*)
 !!$  DO k=1,m
 !!$     WRITE(*,*) (hmat_work(k,j),j=1,m)
@@ -5869,53 +5869,53 @@ CALL mpro%allgather(scalprod_pleg(:,:,:,ispec), scalprod_pleg)
 !!$print *,'matrix hmat'
 !!$pause
 !
-  ALLOCATE(work(1))
-  lwork=-1
-!
-  CALL zhseqr('E','N',m,1,m,hmat_work,m,ritznum,hmat_work,m,work,lwork,info)
-!
-  IF(info.NE.0) THEN
-    IF(info.GT.0) THEN
-       PRINT *,'arnoldi: zhseqr failed to compute all eigenvalues'
-       PRINT *,'info: ',info
-    ELSE
-      PRINT *,'arnoldi: argument ',-info,' has illigal value in zhseqr' 
+    ALLOCATE(work(1))
+    lwork=-1
+
+    CALL zhseqr('E','N',m,1,m,hmat_work,m,ritznum,hmat_work,m,work,lwork,info)
+
+    IF(info.NE.0) THEN
+      IF(info.GT.0) THEN
+         PRINT *,'arnoldi: zhseqr failed to compute all eigenvalues'
+         PRINT *,'info: ',info
+      ELSE
+        PRINT *,'arnoldi: argument ',-info,' has illigal value in zhseqr'
+      ENDIF
+      DEALLOCATE(hmat_work,work)
+      ierr=1
+      RETURN
     ENDIF
-    DEALLOCATE(hmat_work,work)
-    ierr=1
-    RETURN
-  ENDIF
-!
-  lwork=work(1)
-  DEALLOCATE(work)
-  ALLOCATE(work(lwork))
-!
-  CALL zhseqr('E','N',m,1,m,hmat_work,m,ritznum,hmat_work,m,work,lwork,info)
-!
-  IF(info.NE.0) THEN
-    IF(info.GT.0) THEN
-      PRINT *,'arnoldi: zhseqr failed to compute all eigenvalues'
-    ELSE
-      PRINT *,'arnoldi: argument ',-info,' has illigal value in zhseqr' 
+
+    lwork=work(1)
+    DEALLOCATE(work)
+    ALLOCATE(work(lwork))
+
+    CALL zhseqr('E','N',m,1,m,hmat_work,m,ritznum,hmat_work,m,work,lwork,info)
+
+    IF(info.NE.0) THEN
+      IF(info.GT.0) THEN
+        PRINT *,'arnoldi: zhseqr failed to compute all eigenvalues'
+      ELSE
+        PRINT *,'arnoldi: argument ',-info,' has illigal value in zhseqr'
+      ENDIF
+      DEALLOCATE(hmat_work,work)
+      ierr=1
+      RETURN
     ENDIF
-    DEALLOCATE(hmat_work,work)
-    ierr=1
-    RETURN
-  ENDIF
 
     ! Sort eigenvalues acording to magnitude.
-  DO k=1,m
-    info=0
-    DO j=2,m
-      IF(ABS(ritznum(j)).GT.ABS(ritznum(j-1))) THEN
-        info=1
-        tmp=ritznum(j-1)
-        ritznum(j-1)=ritznum(j)
-        ritznum(j)=tmp
-      ENDIF
+    DO k=1,m
+      info=0
+      DO j=2,m
+        IF(ABS(ritznum(j)).GT.ABS(ritznum(j-1))) THEN
+          info=1
+          tmp=ritznum(j-1)
+          ritznum(j-1)=ritznum(j)
+          ritznum(j)=tmp
+        ENDIF
+      ENDDO
+      IF(info.EQ.0) EXIT
     ENDDO
-    IF(info.EQ.0) EXIT
-  ENDDO
 
     ! Sort eigenvalues so that for conjugate pairs, the one with imaginary
     ! part > 0 comes first.
@@ -5931,37 +5931,36 @@ CALL mpro%allgather(scalprod_pleg(:,:,:,ispec), scalprod_pleg)
       end if
     end do
 
-  DEALLOCATE(work)
-!
-! compute how many eigenvalues exceed the tolerance (TOL):
-!
-  ALLOCATE(selec(m),rnum(m))
-  selec=.FALSE.
-  ngrow=0
-  DO j=1,m
-    IF(ABS(ritznum(j)).LT.tol) EXIT
-    ngrow=ngrow+1
-    selec(j)=.TRUE.
-  ENDDO
-  rnum=ritznum
-  hmat_work=hmat
-  ALLOCATE(work(m*m),rwork(m),ifailr(m))
-  eigh=(0.d0,0.d0)
-!
-  CALL zhsein('R','Q','N',selec,m,hmat_work,m,rnum,rnum,1,eigh(:,1:ngrow),m,  &
+    DEALLOCATE(work)
+
+    ! compute how many eigenvalues exceed the tolerance (TOL):
+    ALLOCATE(selec(m),rnum(m))
+    selec=.FALSE.
+    ngrow=0
+    DO j=1,m
+      IF(ABS(ritznum(j)).LT.tol) EXIT
+      ngrow=ngrow+1
+      selec(j)=.TRUE.
+    ENDDO
+    rnum=ritznum
+    hmat_work=hmat
+    ALLOCATE(work(m*m),rwork(m),ifailr(m))
+    eigh=(0.d0,0.d0)
+
+    CALL zhsein('R','Q','N',selec,m,hmat_work,m,rnum,rnum,1,eigh(:,1:ngrow),m,  &
               ngrow,ngrow,work,rwork,ifailr,ifailr,info)
-!
-  IF(info.NE.0) THEN
-    IF(info.GT.0) THEN
-      PRINT *,'arnoldi: ',info,' eigenvectors not converged in zhsein'
-    ELSE
-      PRINT *,'arnoldi: argument ',-info,' has illigal value in zhsein' 
+
+    IF(info.NE.0) THEN
+      IF(info.GT.0) THEN
+        PRINT *,'arnoldi: ',info,' eigenvectors not converged in zhsein'
+      ELSE
+        PRINT *,'arnoldi: argument ',-info,' has illigal value in zhsein'
+      ENDIF
+      ierr=1
     ENDIF
-    ierr=1
-  ENDIF
-!
-  DEALLOCATE(hmat_work,work,rwork,selec,rnum,ifailr)
-!
+
+    DEALLOCATE(hmat_work,work,rwork,selec,rnum,ifailr)
+
   END SUBROUTINE try_eigvecvals
 
   subroutine save_qflux_symm_allspec()
