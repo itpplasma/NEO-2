f26e0441ad8e8f33c75544210a4b6835bf8cc603
Rico Buchholz <buchholz@tugraz.at>
2019-08-05 17:22:22 +0200

    Move subroutine flint_prepare to flint_mod.
    This subroutine was the same in both versions, thus this seems like a
    good idea to reduce code doubling.

--- NEO-2-QL/neo2.f90
+++ NEO-2-QL/neo2.f90
@@ -13,7 +13,7 @@ PROGRAM neo2
        bsfunc_local_err_max_mult,bsfunc_max_mult_reach,             &
        bsfunc_modelfunc_num,bsfunc_divide,                          &
        bsfunc_ignore_trap_levels,boundary_dist_limit_factor,        &
-       bsfunc_local_shield_factor,bsfunc_shield
+       bsfunc_local_shield_factor,bsfunc_shield, flint_prepare
   USE device_mod
   USE collisionality_mod, ONLY : conl_over_mfp,isw_lorentz,         &
        isw_integral,isw_energy,isw_axisymm,                         &
