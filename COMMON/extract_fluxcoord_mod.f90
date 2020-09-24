ab095a77d684afc865bc03d4df6d51abf169704e
Rico Buchholz <buchholz@tugraz.at>
2020-09-24 12:37:22 +0200

    Remove files that should now be used via libneo.
    Remove files from neo-2 which are part of libneo, as those versions
    should be used.

--- COMMON/extract_fluxcoord_mod.f90
+++
@@ -1,7 +0,0 @@
-module extract_fluxcoord_mod
-  integer :: load_extract_fluxcoord=1
-  integer :: nphinorm
-  double precision :: psif_extract,theta_extract,psifmin,hpsif
-  double precision :: psifmax,phifmax,sigcos
-  double precision, dimension(:), allocatable :: phinorm_arr
-end module extract_fluxcoord_mod
