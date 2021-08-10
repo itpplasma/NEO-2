1d9c3b97ff1be7d131ea51cc14deff1603b7a819
Rico Buchholz <buchholz@tugraz.at>
2021-08-10 12:12:57 +0200

    Minor, adapt style of script.
    This includes:
    - spaces instead of tabs
    - if __name__ clause
    - use of python3

--- PythonScripts/create_surf.py
+++ PythonScripts/create_surf.py
@@ -1,67 +1,69 @@
-#!/usr/bin/env python
-import sys
-import numpy as np
-import shutil, errno
-import subprocess
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-surf_dir_name = "s"
+if __name__ == "__main__":
+  import numpy as np
+  import shutil
+  import subprocess
 
-s_vec, collpar_vec, Z_vec, T_vec = np.loadtxt('surfaces.dat', unpack=True)
+  surf_dir_name = "s"
 
-print "Should I create the surface directories [y,n]?"
-ans = raw_input().lower()
-print "Relativistic?"
-rel = raw_input().lower()
+  s_vec, collpar_vec, Z_vec, T_vec = np.loadtxt('surfaces.dat', unpack=True)
+
+  print "Should I create the surface directories [y,n]?"
+  ans = raw_input().lower()
+  print "Relativistic?"
+  rel = raw_input().lower()
 
 
 
-#ans = 'y'
-if ans == 'y':
-  k = 0
-  for s in s_vec:
-    #dir_name = surf_dir_name + str(k+1)
-    dir_name = surf_dir_name + ('%.9e' %(s_vec[k])).replace('e', 'd').replace('d-', 'm')
-    print dir_name
-    shutil.rmtree(dir_name, ignore_errors=True)
-    shutil.copytree('TEMPLATE_DIR', dir_name, symlinks=True)
-    f = open(dir_name + "/neo2.in", "rt")
-    neo2 = f.read()
-    f.close()
+  #ans = 'y'
+  if ans == 'y':
+    k = 0
+    for s in s_vec:
+      #dir_name = surf_dir_name + str(k+1)
+      dir_name = surf_dir_name + ('%.9e' %(s_vec[k])).replace('e', 'd').replace('d-', 'm')
+      print dir_name
+      shutil.rmtree(dir_name, ignore_errors=True)
+      shutil.copytree('TEMPLATE_DIR', dir_name, symlinks=True)
+      f = open(dir_name + "/neo2.in", "rt")
+      neo2 = f.read()
+      f.close()
 
-    neo2 = neo2.replace('<conl_over_mfp>', ('%.9e' %(collpar_vec[k])).replace('e', 'd'))
-    neo2 = neo2.replace('<boozer_s>',      ('%.9e' %(s_vec[k])).replace('e', 'd'))
-    neo2 = neo2.replace('<z_eff>',         ('%.9e' %(Z_vec[k])).replace('e', 'd'))
-    if rel == 'y':
-      neo2 = neo2.replace('<T_e>',         ('%.9e' %(T_vec[k])).replace('e', 'd'))
+      neo2 = neo2.replace('<conl_over_mfp>', ('%.9e' %(collpar_vec[k])).replace('e', 'd'))
+      neo2 = neo2.replace('<boozer_s>',      ('%.9e' %(s_vec[k])).replace('e', 'd'))
+      neo2 = neo2.replace('<z_eff>',         ('%.9e' %(Z_vec[k])).replace('e', 'd'))
+      if rel == 'y':
+        neo2 = neo2.replace('<T_e>',         ('%.9e' %(T_vec[k])).replace('e', 'd'))
 
-    f = open(dir_name + "/neo2.in", "wt")
-    f.write(neo2)
-    f.close()
-    k = k + 1
+      f = open(dir_name + "/neo2.in", "wt")
+      f.write(neo2)
+      f.close()
+      k = k + 1
 
 #print "Should I submit the jobs to condor [y,n]?"
 #ans = raw_input().lower()
 #if ans == 'y':
-# k = 0
-# for s in s_desired:
-#   dir_name = surf_dir_name + str(k+1)
-#   p = subprocess.Popen(["condor_submit_dag", "batch.dag"], cwd=dir_name)
-#   p.wait()
-#   k = k + 1
+#  k = 0
+#  for s in s_desired:
+#    dir_name = surf_dir_name + str(k+1)
+#    p = subprocess.Popen(["condor_submit_dag", "batch.dag"], cwd=dir_name)
+#    p.wait()
+#    k = k + 1
 
 
 #print "Should I prepare jobfiles for DCluster [y,n]?"
 #ans = raw_input().lower()
 #if ans == 'y':
-# k = 0
-# for s in s_vec:
-#   for j in range(0,3):
-#     dir_name = surf_dir_name + str(k+1)
-#     fin = open(dir_name + "/NEO2-"+str(j)+".job", "rt")
-#     jobfile = fin.read()
-#     jobfile = jobfile.replace('<jobname>', 'Neo2-' + dir_name)
-#     fin.close()
-#     fout = open(dir_name + "/NEO2-"+str(j)+".job", "wt")
-#     fout.write(jobfile)
-#     fout.close()
-#   k = k + 1
+#  k = 0
+#  for s in s_vec:
+#    for j in range(0,3):
+#      dir_name = surf_dir_name + str(k+1)
+#      fin = open(dir_name + "/NEO2-"+str(j)+".job", "rt")
+#      jobfile = fin.read()
+#      jobfile = jobfile.replace('<jobname>', 'Neo2-' + dir_name)
+#      fin.close()
+#      fout = open(dir_name + "/NEO2-"+str(j)+".job", "wt")
+#      fout.write(jobfile)
+#      fout.close()
+#    k = k + 1
