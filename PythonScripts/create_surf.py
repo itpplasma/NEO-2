f4ec98650e30f51dcebe631616f5be1de6d6547c
Rico Buchholz <buchholz@tugraz.at>
2021-08-19 15:31:09 +0200

    Remove questions from script.
    Question for relativistic part seems unneccessary, as it will do nothing
    if the sequence to replace is not present. If it is present in a
    non-relativistic run, the parameter should be unused in the code, but if
    it is not a number it should lead to an error when trying to read the
    namelist.
    
    Question wether to create surfaces was deemed unneccessary, as this is
    the whole purpose of the script.

--- PythonScripts/create_surf.py
+++ PythonScripts/create_surf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Interactive script to create flux surface subfolders of neo-2-par run.
+Script to create flux surface subfolders of neo-2-par run.
 """
 
 if __name__ == "__main__":
@@ -13,33 +13,23 @@ if __name__ == "__main__":
 
   s_vec, collpar_vec, Z_vec, T_vec = np.loadtxt('surfaces.dat', unpack=True)
 
-  print("Should I create the surface directories [y,n]?")
-  ans = input().lower()
-  print("Relativistic?")
-  rel = input().lower()
+  k = 0
+  for s in s_vec:
+    #dir_name = surf_dir_name + str(k+1)
+    dir_name = surf_dir_name + ('%.9e' %(s_vec[k])).replace('e', 'd').replace('d-', 'm')
+    print(dir_name)
+    shutil.rmtree(dir_name, ignore_errors=True)
+    shutil.copytree('TEMPLATE_DIR', dir_name, symlinks=True)
+    f = open(dir_name + "/neo2.in", "rt")
+    neo2 = f.read()
+    f.close()
 
+    neo2 = neo2.replace('<conl_over_mfp>', ('%.9e' %(collpar_vec[k])).replace('e', 'd'))
+    neo2 = neo2.replace('<boozer_s>',      ('%.9e' %(s_vec[k])).replace('e', 'd'))
+    neo2 = neo2.replace('<z_eff>',         ('%.9e' %(Z_vec[k])).replace('e', 'd'))
+    neo2 = neo2.replace('<T_e>',         ('%.9e' %(T_vec[k])).replace('e', 'd'))
 
-
-  #ans = 'y'
-  if ans == 'y':
-    k = 0
-    for s in s_vec:
-      #dir_name = surf_dir_name + str(k+1)
-      dir_name = surf_dir_name + ('%.9e' %(s_vec[k])).replace('e', 'd').replace('d-', 'm')
-      print(dir_name)
-      shutil.rmtree(dir_name, ignore_errors=True)
-      shutil.copytree('TEMPLATE_DIR', dir_name, symlinks=True)
-      f = open(dir_name + "/neo2.in", "rt")
-      neo2 = f.read()
-      f.close()
-
-      neo2 = neo2.replace('<conl_over_mfp>', ('%.9e' %(collpar_vec[k])).replace('e', 'd'))
-      neo2 = neo2.replace('<boozer_s>',      ('%.9e' %(s_vec[k])).replace('e', 'd'))
-      neo2 = neo2.replace('<z_eff>',         ('%.9e' %(Z_vec[k])).replace('e', 'd'))
-      if rel == 'y':
-        neo2 = neo2.replace('<T_e>',         ('%.9e' %(T_vec[k])).replace('e', 'd'))
-
-      f = open(dir_name + "/neo2.in", "wt")
-      f.write(neo2)
-      f.close()
-      k = k + 1
+    f = open(dir_name + "/neo2.in", "wt")
+    f.write(neo2)
+    f.close()
+    k = k + 1
