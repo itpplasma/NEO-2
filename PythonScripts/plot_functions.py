df41d3cec96dc81d619b60b9c771792cbfbd552c
Rico Buchholz <buchholz@tugraz.at>
2021-09-22 16:53:34 +0200

    Changed Plotfunctions to Methods
    On behalf of Michael Draxler.

--- PythonScripts/plot_functions.py
+++ PythonScripts/plot_functions.py
@@ -5,12 +5,13 @@ Created on Tue Feb 11 14:52:59 2020
 
 @author: wakatobi
 """
-__version__='0.0.1'
+__version__='0.0.5'
 import os
 import matplotlib.pyplot as plt
 import h5py
 import numpy as np
 from matplotlib.figure import Figure
+from scipy.interpolate import interp2d
 
 class Neo2Files:
     """Class of File for Neo2 Runs""" # Abstract class usefull?
@@ -104,9 +105,13 @@ def plot_g(path):
 
 class Plotfunctions():
 
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.figures=list()
+    def __init__(self,path2bch5):
+
+        if os.path.isfile(path2bch5):
+            self.path2bch5=path2bch5
+        else:
+            raise IOError('Must be a file')
+        self.figures=[]
 
     def add_fig(self):
         self.figures.append(plt.figure())
@@ -118,13 +123,13 @@ class Plotfunctions():
 
     def peak_into_dir(self):
         pass
-    def plot_boozer(self, pathtobch5,new_figure=True):
+    def plot_boozer(self,new_figure=True):
         """Plots different representations of boozerfile
 
         according to the choosen surface
         """
 
-        bch5=h5py.File(pathtobch5,'r')
+        bch5=h5py.File(self.path2bch5,'r')
         theta_n=bch5['theta_n'].value
         theta=np.array(bch5['theta'])
         phi_n=bch5['phi_n'].value
@@ -136,8 +141,7 @@ class Plotfunctions():
 
         if new_figure:
             self.add_fig()
-        gcf=plt.gcf()
-        axs =gcf.subplots(1, 1)
+        axs=plt.gca()
         magfield=axs.pcolor(phi,theta,bmod.T)
         cbar=plt.colorbar(magfield)
         axs.set_ylabel(r'$\theta$')
@@ -149,125 +153,123 @@ class Plotfunctions():
     #oder
     #plt.contourf(phi,theta,bmod.T,20)
 
-def plot_boozer_fieldline(pathtobch5,len_phi=12,phistart=0,thetastart=0,new_figure=True):
-    """Plots the magnetic fieldline in inside the surface"""
+    def plot_boozer_fieldline(self,len_phi=12,phistart=0,thetastart=0,new_figure=True):
+        """Plots the magnetic fieldline in inside the surface"""
 
-    if new_figure:
-        fig,ax =plt.subplots()
-    plot_boozer(pathtobch5,new_figure=False)
-
-
-    bch5=h5py.File(pathtobch5,'r')
-
-    theta=np.array(bch5['theta'])
-    phi=np.array(bch5['phi'])
-
-    maxphi=max(phi)
-    maxtheta=max(theta)
-    iota=bch5['iota'].value
-    bch5.close()
+        if new_figure:
+            fig,ax =plt.subplots()
+        self.plot_boozer(new_figure=False)
 
 
+        bch5=h5py.File(self.path2bch5,'r')
 
-    plt.plot(phistart,thetastart,'bo')
+        theta=np.array(bch5['theta'])
+        phi=np.array(bch5['phi'])
+
+        maxphi=max(phi)
+        maxtheta=max(theta)
+        iota=bch5['iota'].value
+        bch5.close()
+
+
+
+        plt.plot(phistart,thetastart,'bo')
 
 ############## Fieldline plotting ############################################
 
-    phiend=(len_phi+phistart)
-    while phiend>maxphi:
-        thetaend=(thetastart+(maxphi-phistart)*iota)# Total Korrekt
+        phiend=(len_phi+phistart)
+        while phiend>maxphi:
+            thetaend=(thetastart+(maxphi-phistart)*iota)# Total Korrekt
 
-        while thetaend>=maxtheta: # Korrekt
+            while thetaend>=maxtheta: # Korrekt
+                phiend_i=(maxtheta-thetastart)/iota+phistart
+                plt.plot([phistart,phiend_i],[thetastart,maxtheta],'r')
+                phistart=phiend_i
+                thetaend=thetaend-maxtheta
+                thetastart=0
+
+            plt.plot([phistart,maxphi],[thetastart,thetaend],'r')
+            thetastart=thetaend
+            phiend=phiend-maxphi
+            phistart=0
+
+
+        thetaend=thetastart+phiend*iota
+
+
+        ###### Same as above ###################
+        while thetaend>=maxtheta:
             phiend_i=(maxtheta-thetastart)/iota+phistart
             plt.plot([phistart,phiend_i],[thetastart,maxtheta],'r')
             phistart=phiend_i
             thetaend=thetaend-maxtheta
             thetastart=0
-
-        plt.plot([phistart,maxphi],[thetastart,thetaend],'r')
-        thetastart=thetaend
-        phiend=phiend-maxphi
-        phistart=0
+        ########################################
 
 
-    thetaend=thetastart+phiend*iota
-
-
-    ###### Same as above ###################
-    while thetaend>=maxtheta:
-        phiend_i=(maxtheta-thetastart)/iota+phistart
-        plt.plot([phistart,phiend_i],[thetastart,maxtheta],'r')
-        phistart=phiend_i
-        thetaend=thetaend-maxtheta
-        thetastart=0
-    ########################################
-
-
-    plt.plot([phistart,phiend],[thetastart,thetaend],'r')
-    #plt.show()
-    #return ax
+        plt.plot([phistart,phiend],[thetastart,thetaend],'r')
+        #plt.show()
+        #return ax
 ##############################################################################
-from scipy.interpolate import interp2d
-
-
-def plot_boozer_fieldline_poi(pathtobch5,poi,len_phi=12,phistart=0,thetastart=0,new_figure=True):
-    """Plots Point of Intererst into the fieldline"""
-
-    plt.figure()
-    plt.subplot(211)
-    ax=plot_boozer_fieldline(pathtobch5,len_phi,phistart,thetastart,new_figure=False)
 
 
 
-    bch5=h5py.File(pathtobch5,'r')
+    def plot_boozer_fieldline_poi(self,poi,len_phi=12,phistart=0,thetastart=0,new_figure=True):
+        """Plots Point of Intererst into the fieldline"""
+
+        plt.figure()
+        plt.subplot(211)
+        self.plot_boozer_fieldline(len_phi,phistart,thetastart,new_figure=False)
 
 
-    theta_n=bch5['theta_n'].value
-    theta=np.array(bch5['theta'])
-    phi_n=bch5['phi_n'].value
-    phi=np.array(bch5['phi'])
-    bmod=np.array(bch5['bmod'])
-    bmod=bmod.reshape(phi_n+1,theta_n+1)
 
-    fit=interp2d(phi,theta,bmod.T)
-    maxphi=max(phi)
-    maxtheta=max(theta)
-    iota=bch5['iota'].value
-    bch5.close()
+        bch5=h5py.File(self.path2bch5,'r')
 
 
-    plot_boozer_poi(poi,iota,maxphi,maxtheta,phistart,thetastart)
+        theta_n=bch5['theta_n'].value
+        theta=np.array(bch5['theta'])
+        phi_n=bch5['phi_n'].value
+        phi=np.array(bch5['phi'])
+        bmod=np.array(bch5['bmod'])
+        bmod=bmod.reshape(phi_n+1,theta_n+1)
 
-    plt.subplot(212)
-
-    def get_B_phiS(bs_l,iota,maxphi,maxtheta,phistart,thetastart):
-
-        bx_l=bs_l/np.sqrt(1+iota*iota)
-        by_l=bx_l*iota
-        bx_l=bx_l+phistart
-        by_l=by_l+thetastart
-        bz_l=[]
-        for i,j in zip(bx_l%maxphi,by_l%maxtheta):
-            bz_l.append(fit(i,j))
-        bz_l=np.array(bz_l).flatten()
-        return(bz_l)
-
-    bs_l=np.linspace(0,len_phi,1000)*np.sqrt(1+iota*iota)
-
-    bz_l=get_B_phiS(bs_l,iota,maxphi,maxtheta,phistart,thetastart)
+        fit=interp2d(phi,theta,bmod.T)
+        maxphi=max(phi)
+        maxtheta=max(theta)
+        iota=bch5['iota'].value
+        bch5.close()
 
 
-    plt.plot(bs_l,bz_l,'r')
-    bz_p=get_B_phiS(poi,iota,maxphi,maxtheta,phistart,thetastart)
-    plt.plot(poi,bz_p,'+')
-    for i,j in zip(poi,bz_p):
-        plt.annotate('%s'%i,xy=(i,j))
+        plot_boozer_poi(poi,iota,maxphi,maxtheta,phistart,thetastart)
+
+        plt.subplot(212)
+
+        def get_B_phiS(bs_l,iota,maxphi,maxtheta,phistart,thetastart):
+
+            bx_l=bs_l/np.sqrt(1+iota*iota)
+            by_l=bx_l*iota
+            bx_l=bx_l+phistart
+            by_l=by_l+thetastart
+            bz_l=[]
+            for i,j in zip(bx_l%maxphi,by_l%maxtheta):
+                bz_l.append(fit(i,j))
+            bz_l=np.array(bz_l).flatten()
+            return(bz_l)
+
+        bs_l=np.linspace(0,len_phi,1000)*np.sqrt(1+iota*iota)
+
+        bz_l=get_B_phiS(bs_l,iota,maxphi,maxtheta,phistart,thetastart)
 
 
-    plt.ylabel(r'$B$')
-    plt.xlabel(r'$\varphi_s$')
+        plt.plot(bs_l,bz_l,'r')
+        bz_p=get_B_phiS(poi,iota,maxphi,maxtheta,phistart,thetastart)
+        plt.plot(poi,bz_p,'+')
+        for i,j in zip(poi,bz_p):
+            plt.annotate('%s'%i,xy=(i,j))
 
 
+        plt.ylabel(r'$B$')
+        plt.xlabel(r'$\varphi_s$')
 
 def plot_boozer_poi(poi,iota,maxphi,maxtheta,phistart=0,thetastart=0):
     """Plots Point of Interest into the boozer representation"""
@@ -282,6 +284,8 @@ def plot_boozer_poi(poi,iota,maxphi,maxtheta,phistart=0,thetastart=0):
         plt.annotate('%s'%i,xy=(j,k))
     plt.ylabel(r'$\theta$')
     plt.xlabel(r'$\varphi$')
+
+
 
 
 
