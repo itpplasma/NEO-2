38fd67027b37270e37383120b8c5ee4f9bd8a52f
Rico Buchholz <buchholz@tugraz.at>
2021-11-08 17:50:08 +0100

    Use neo_actual_spectra of common folder.
    Tool create_surfaces now uses the neo_actual_spectra of the COMMON
    folder, instead of an own version.

--- tools/create_surfaces/src/neo_actual_spectra.f90
+++
@@ -1,6 +0,0 @@
-MODULE neo_actual_spectra
-! Actual spectra on flux surface
-  use nrtype, only : dp
-  REAL(kind=dp),    DIMENSION(:),     ALLOCATABLE :: s_rmnc, s_zmnc, s_lmnc
-  REAL(kind=dp),    DIMENSION(:),     ALLOCATABLE :: s_bmnc
-END MODULE neo_actual_spectra
