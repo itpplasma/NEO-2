390ebf78505704c92644ad8f38f2fe4b1455428b
Rico Buchholz <buchholz@tugraz.at>
2021-11-09 17:41:54 +0100

    Use sizey_bo/cur of common folder.
    Tool create_surfaces now uses the sizey_bo/cur of the COMMON
    folder, instead of an own version.

--- tools/create_surfaces/src/sizey_cur.f90
+++
@@ -1,8 +0,0 @@
-MODULE sizey_cur
-  use nrtype, only : dp
-! Definition for rk4d_bo also used in main routine neo
-  INTEGER            ::  npart_cur
-  INTEGER            ::  ndim_cur
-  INTEGER, PARAMETER ::  npq_cur = 11
-  INTEGER            ::  alpha_cur
-END MODULE sizey_cur
