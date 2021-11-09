ca7e0d492a4cd626df51ded0f59d19dd71c4e3ab
Rico Buchholz <buchholz@tugraz.at>
2021-11-09 18:31:48 +0100

    Rename to match module name.
    File and module name differed, which makes it harder to find.

--- COMMON/spline_mod.f90
+++ COMMON/spline_mod.f90
@@ -6,7 +6,7 @@ SUBROUTINE spl2d(nx,ny,hx,hy,mx,my,f,spl)
 
 ! Makes a 2-dimensional cubic spline of function f(x,y)
 !
-! Input:  nx, ny              number of values in x and y  
+! Input:  nx, ny              number of values in x and y
 !         hx, hy              step size in x and y (aequidistant)
 !         mx, my              spline mode (0: standard, 1: periodic)
 !         f(nx,ny)            f(x,y)-values
@@ -20,7 +20,7 @@ SUBROUTINE spl2d(nx,ny,hx,hy,mx,my,f,spl)
   REAL(kind=dp),                       INTENT(in)  :: hx, hy
   REAL(kind=dp), DIMENSION(nx,ny)    , INTENT(in)  :: f
   REAL(kind=dp), DIMENSION(4,4,nx,ny), INTENT(out) :: spl
-  
+
   REAL(kind=dp), DIMENSION(:),         ALLOCATABLE :: bi, ci, di, s
   INTEGER                                          :: i, j, k
 
@@ -70,16 +70,16 @@ SUBROUTINE eva2d(nx,ny,ix,iy,dx,dy,spl,spval)
 
 ! Evaluates a 2-dimensional cubic spline of function f(x,y)
 !
-! Input:  nx, ny              number of values in x and y  
+! Input:  nx, ny              number of values in x and y
 !         ix, iy              pointer into the spline array spl
 !         dx, dy              distance from x(ix) and y(iy)
 !         spl                 array with spline data
 ! Output: spval               evaluated function value
 
   USE neo_precision
-  
+
   IMPLICIT NONE
-  
+
   INTEGER,                             INTENT(in)  :: nx, ny, ix, iy
   REAL(kind=dp),                       INTENT(in)  :: dx, dy
   REAL(kind=dp), DIMENSION(4,4,nx,ny), INTENT(in)  :: spl
@@ -89,7 +89,7 @@ SUBROUTINE eva2d(nx,ny,ix,iy,dx,dy,spl,spval)
   INTEGER                                          :: l
 
   DO l=1,4
-     a(l) = spl(1,l,ix,iy) + dx*(spl(2,l,ix,iy) +              & 
+     a(l) = spl(1,l,ix,iy) + dx*(spl(2,l,ix,iy) +              &
           dx*(spl(3,l,ix,iy) + dx* spl(4,l,ix,iy)))
   END DO
   spval = a(1)+dy*(a(2)+dy*(a(3)+dy*a(4)))
@@ -101,7 +101,7 @@ SUBROUTINE eva2d_fd(nx,ny,ix,iy,dx,dy,spl,spval)
 
 ! Evaluates the first derivatives of 2-dimensional cubic spline of function f(x,y)
 !
-! Input:  nx, ny              number of values in x and y  
+! Input:  nx, ny              number of values in x and y
 !         ix, iy              pointer into the spline array spl
 !         dx, dy              distance from x(ix) and y(iy)
 !         spl                 array with spline data
@@ -110,9 +110,9 @@ SUBROUTINE eva2d_fd(nx,ny,ix,iy,dx,dy,spl,spval)
 !                             spval(2) = df/dy
 
   USE neo_precision
-  
+
   IMPLICIT NONE
-  
+
   INTEGER,                             INTENT(in)  :: nx, ny, ix, iy
   REAL(kind=dp),                       INTENT(in)  :: dx, dy
   REAL(kind=dp), DIMENSION(4,4,nx,ny), INTENT(in)  :: spl
@@ -155,7 +155,7 @@ SUBROUTINE eva2d_fd(nx,ny,ix,iy,dx,dy,spl,spval)
            mulj = dy**(j-2)
         END IF
         mulj = mulj * (j-1)
-        spval(2) = spval(2) + spl(i,j,ix,iy) * muli * mulj 
+        spval(2) = spval(2) + spl(i,j,ix,iy) * muli * mulj
      END DO
   END DO
 
@@ -166,7 +166,7 @@ SUBROUTINE eva2d_sd(nx,ny,ix,iy,dx,dy,spl,spval)
 
 ! Evaluates the second derivatives of 2-dimensional cubic spline of function f(x,y)
 !
-! Input:  nx, ny              number of values in x and y  
+! Input:  nx, ny              number of values in x and y
 !         ix, iy              pointer into the spline array spl
 !         dx, dy              distance from x(ix) and y(iy)
 !         spl                 array with spline data
@@ -176,9 +176,9 @@ SUBROUTINE eva2d_sd(nx,ny,ix,iy,dx,dy,spl,spval)
 !                             spval(3) = d^2f/dy^2
 
   USE neo_precision
-  
+
   IMPLICIT NONE
-  
+
   INTEGER,                             INTENT(in)  :: nx, ny, ix, iy
   REAL(kind=dp),                       INTENT(in)  :: dx, dy
   REAL(kind=dp), DIMENSION(4,4,nx,ny), INTENT(in)  :: spl
@@ -251,7 +251,7 @@ SUBROUTINE splreg(n,h,y,bi,ci,di)
 
 ! Makes a cubic spline of function y(x)
 !
-! Input:  n                   number of values in y  
+! Input:  n                   number of values in y
 !         h                   step size in x (aequidistant)
 !         y(n)                y-values
 ! Output: bi(n),ci(n),di(n)   Spline parameters
@@ -325,7 +325,7 @@ SUBROUTINE splper(n,h,y,bi,ci,di)
   INTEGER                                  :: nmx, n1, n2, i, i1
 
   ALLOCATE ( bmx(n), yl(n), amx1(n), amx2(n), amx3(n) )
-  
+
   bmx(1) = 1.d30
 
   nmx=n-1
@@ -423,7 +423,7 @@ SUBROUTINE poi2d(hx,hy,mx,my,                         &
      x,y,ix,iy,dx,dy,ierr)
 ! Creates Pointers for eva2d
 !
-! Input:  hx, hy              increment in x and y  
+! Input:  hx, hy              increment in x and y
 !         mx, my              standard (0) or periodic (1) spline
 !         xmin, xmax          Minimum and maximum x
 !         ymin, ymax          Minimum and maximum y
@@ -501,8 +501,3 @@ SUBROUTINE poi2d(hx,hy,mx,my,                         &
 END SUBROUTINE poi2d
 
 END MODULE spline_mod
-
-
-
-
-
