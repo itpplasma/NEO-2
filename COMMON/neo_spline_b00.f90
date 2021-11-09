5ef32a701f00fd42495887c10dcee3e13ee5d362
Rico Buchholz <buchholz@tugraz.at>
2021-11-09 10:28:31 +0100

    Avoid indirection in module use.
    Use module nrtype (instead of some module which just does use nrtype).
    
    Preperation to remove the create_surfaces version of neo_spline_b00.

--- COMMON/neo_spline_b00.f90
+++ COMMON/neo_spline_b00.f90
@@ -1,6 +1,6 @@
 MODULE neo_spline_b00
   ! spline for b00
-  USE neo_precision
+  use nrtype, only : dp
   REAL(kind=dp), DIMENSION(:),   ALLOCATABLE :: a_b00, b_b00, c_b00, d_b00
 
 END MODULE neo_spline_b00
