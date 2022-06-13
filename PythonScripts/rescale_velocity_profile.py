c151f4ae53d6409204f73bb010c6689dcbf788c6
Rico Buchholz <buchholz@tugraz.at>
2022-06-13 10:27:00 +0200

    New function to rescale density.
    Add a function to rescale density and density gradient.
    
    Should probably be generalized to avoid the code doubling.

--- PythonScripts/rescale_velocity_profile.py
+++ PythonScripts/rescale_velocity_profile.py
@@ -53,3 +53,50 @@ def rescale_velocity_profile(path:str, infilename:str, scaling_factor:float):
   dset[...] = vphi_rescaled
 
   vphi_rs.close()
+
+def rescale_density_profile(path:str, infilename:str, scaling_factor:float):
+  """
+  From a given neo2 hdf5-input file, create a new one with the density
+  profile rescaled.
+
+  input:
+  ------
+  path,str: string with path where the input file can be found.
+  infilename: string with the name of the input file to use.
+  scaling_factor: floating point number, scaling factor to apply to the
+    density. Expected to be a single number applied to all radial
+    positions.
+
+  \todo Move to hdf5tools?
+  """
+  import h5py, hdf5tools
+  import numpy as np
+
+  ref = hdf5tools.get_hdf5file(path+infilename)
+
+  n_prof = np.array(ref['n_prof'])
+  n_prof_rescaled = n_prof*scaling_factor
+
+  dn_prof = np.array(ref['dn_ov_ds_prof'])
+  dn_prof_rescaled = dn_prof*scaling_factor
+
+  nameparts = infilename.rsplit('.', 1)
+
+  n_namepart= '_n_{0}'.format(scaling_factor)
+
+  if len(nameparts)==1:
+    outfilename = nameparts[0] + n_namepart
+  elif len(nameparts)==2:
+    outfilename = nameparts[0] + n_namepart+'.'+nameparts[1]
+
+  n_rs = hdf5tools.get_hdf5file_replace(outfilename)
+
+  for k in ref.keys():
+    ref.copy(source='/'+k, dest=n_rs, name='/' + k)
+
+  dset = n_rs['n_prof']
+  dset[...] = n_prof_rescaled
+  dset = n_rs['dn_ov_ds_prof']
+  dset[...] = dn_prof_rescaled
+
+  n_rs.close()
