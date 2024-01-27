827a8c1e15ed09da5728fa341e80dad7eb0a1e3e
GeorgGrassler <georg.grassler@tugraz.at>
2024-01-27 20:17:47 +0100

    Extended setup_retry_of_failed_runs_PAR_VERSION.py * Improved/standartized naming convention * Fixed bug concerning scientific notation: the exponent in the folder name (number after m) was ignored up to this version. This caused problems for labels < 0.1 * Adjusted default value of eps * Better output to consol of progress

--- PythonScripts/setup_retry_of_failed_runs_PAR_VERSION.py
+++ PythonScripts/setup_retry_of_failed_runs_PAR_VERSION.py
@@ -5,7 +5,7 @@
 ###########################################FUNCTIONS###############################################
 ###################################################################################################
 
-def get_list_unsucessful_runs_par(folder: str, subfolder_pattern: str, file_to_check: str):
+def get_list_failed_runs_par(folder: str, subfolder_pattern: str, file_to_check: str):
   """
   \brief Return a list of unsucessful runs.
 
@@ -38,7 +38,7 @@ def get_list_unsucessful_runs_par(folder: str, subfolder_pattern: str, file_to_c
   from pathlib import Path
   import glob as g
 
-  unsucessful_runs = []
+  failed_runs = []
 
   p = Path(folder)
   folders = list(p.glob(subfolder_pattern))
@@ -49,17 +49,17 @@ def get_list_unsucessful_runs_par(folder: str, subfolder_pattern: str, file_to_c
     matching_files = g.glob(current_filename)
 
     if matching_files:
-        unsucessful_runs.append(d.name)
+        failed_runs.append(d.name)
     else:
         continue
 
-  unsucessful_runs.reverse() # Implementation detail - return list in increasing order.
+  failed_runs.reverse() # Implementation detail - return list in increasing order.
 
-  return unsucessful_runs
+  return failed_runs
 
 ###################################################################################################
 
-def append_list_unsucessful_runs_par(list_unsucessful_runs: list, infilename: str, outfilename: str):
+def append_list_failed_runs_par(failed_runs: list, infilename: str, outfilename: str):
   """
   Determine add list of unsucessful runs to file.
 
@@ -72,11 +72,11 @@ def append_list_unsucessful_runs_par(list_unsucessful_runs: list, infilename: st
     a problem for HTcondor and it makes the code simpler.
 
   Example:
-    append_list_unsucessful_runs(list_unsucessful_runs 'submit_failed_template', 'submit_failed')
+    append_list_failed_runs(failed_runs 'submit_failed_template', 'submit_failed')
 
   input:
   ------
-  list_unsucessful_runs: list of strings, containing the names of the
+  failed_runs: list of strings, containing the names of the
     subfolders of the unsucessful runs.
   infilename: string, name (and path) of file to which to append the
     list. Lines after a line with 'Queue' a the begining (whitespace
@@ -100,7 +100,7 @@ def append_list_unsucessful_runs_par(list_unsucessful_runs: list, infilename: st
         if line.split()[0] == 'Queue':
           break
 
-    for run in list_unsucessful_runs:
+    for run in failed_runs:
       if run == 's1.000000000m03':
         # Skip axis
         continue
@@ -111,8 +111,8 @@ def append_list_unsucessful_runs_par(list_unsucessful_runs: list, infilename: st
 ###########################################MAIN####################################################
 ###################################################################################################
 
-def setup_retry_of_failed_runs_PAR_VERSION(faulty_file_pattern: str = "propagator*.h5", use_failed_record: bool = False, 
-                                           eps: float = 1e-10, no_sideeffects: bool = False):
+def setup_retry_of_failed_runs_PAR_VERSION(faulty_file_pattern: str = "propagator*.h5", use_record_of_failed_runs: bool = False, 
+                                           eps: float = 1e-09, no_sideeffects: bool = False):
     """
 
     Make new submit file and reset folders for failed surfaces to retry the runs.
@@ -129,15 +129,15 @@ def setup_retry_of_failed_runs_PAR_VERSION(faulty_file_pattern: str = "propagato
     input:
     ------
     faulty_file_pattern: string, name of the file which PRESENCE indicates a failure.
-    use_failed_record: bool, if True, the list of failed runs is read from record_of_failed_runs.txt
+    use_record_of_failed_runs: bool, if True, the list of failed runs is read from record_of_failed_runs.txt
     eps: float, minimal agreement between entry in surfaces.dat and the name of the detected faulty run 
          in terms of (relative) numerical difference. The folder name is shorter compared to the flux
          label entry in sufaces.dat, which leads to a mismatch in the last digits (rounding of last digit in 
          folder name that can propagate to higher digits as well) The whole logic is first performed only with 
          strings (to avoid conversion erros). However, should the flux label of some of the faulty runs not be
          detected this way, a float number comparison of these not detected cases with the entries in surfaces.dat
-         is performed. Should still no sucessfull match happen, one ought to change eps or check surface.dat by hand.
-         (default: 1e-10)
+         is performed. Should still no sucessfull match happen, one ought to change eps or check surfaces.dat by hand.
+         (default: 1e-09)
     no_sideeffects: bool, if True, only print list of failed runs. No sideeffects.
     
     output:
@@ -154,88 +154,95 @@ def setup_retry_of_failed_runs_PAR_VERSION(faulty_file_pattern: str = "propagato
     # Make surfaces.dat file for failed runs
     import fnmatch
 
-    if not use_failed_record:
-        list_unsucessful_runs = get_list_unsucessful_runs_par("./", "s[1234567890]*", faulty_file_pattern)
+    if not use_record_of_failed_runs:
+        failed_runs = get_list_failed_runs_par("./", "s[1234567890]*", faulty_file_pattern)
     else:
         with open('record_of_failed_runs.txt', 'r') as file:
-            list_unsucessful_runs = file.read().splitlines()
+            failed_runs = file.read().splitlines()
             print('Using record_of_failed_runs.txt to determine failed runs.')
-            print(list_unsucessful_runs)
+            for failed_run in failed_runs:
+                print(failed_run)
 
-    if len(list_unsucessful_runs) == 0:
+    if len(failed_runs) == 0:
         print('No failed runs found. Nothing to do.')
         return
     
     if no_sideeffects:
         print('Only printing list of failed runs. No sideeffects.')
-        print(list_unsucessful_runs)
+        for failed_run in failed_runs:
+                print(failed_run)
         return
     
     # After recreating the folder in later steps, the runs are not considered as failed anymore.
     # Therefore, the list of failed runs is written to a file as backup.
     # Existing entries are not overwritten to further not loose the information.
-    if not use_failed_record:
+    if not use_record_of_failed_runs:
         with open('record_of_failed_runs.txt', 'a') as file:
-            for i in range(len(list_unsucessful_runs)):
-                file.write(list_unsucessful_runs[i] + '\n')
+            for i in range(len(failed_runs)):
+                file.write(failed_runs[i] + '\n')
 
-    list_unsucessful_flux_labels = [run.split('m')[0] + '*' for run in list_unsucessful_runs]
-    list_unsucessful_flux_labels = [run.split('s')[1] for run in list_unsucessful_flux_labels]
-    for i in range(len(list_unsucessful_flux_labels)):
-        parts = list_unsucessful_flux_labels[i].split('.')
-        list_unsucessful_flux_labels[i] = '0.'+parts[0]+parts[1]
+    flux_labels_of_failed_runs = [run.split('m')[0] + '*' for run in failed_runs]
+    flux_labels_of_failed_runs = [flux_label.split('s')[1] for flux_label in flux_labels_of_failed_runs]
+    exponents_of_failed_runs = [int(run.split('m')[1]) for run in failed_runs]
+    for i in range(len(flux_labels_of_failed_runs)):
+        parts = flux_labels_of_failed_runs[i].split('.')
+        flux_labels_of_failed_runs[i] = '0.' + (exponents_of_failed_runs[i]-1)*'0' + parts[0] + parts[1]
 
-    data_to_extract = []
+    found_surfaces_of_failed_runs = []
     with open('surfaces.dat', 'r') as surface_file:
         for line in surface_file:
             columns = line.strip().split(' ')
-            flux_label = columns[0] 
-            if any(fnmatch.fnmatch(flux_label, run) for run in list_unsucessful_flux_labels):
-                data_to_extract.append(line)
+            flux_label_of_line = columns[0] 
+            if any(fnmatch.fnmatch(flux_label_of_line, flux_label) for flux_label in flux_labels_of_failed_runs):
+                found_surfaces_of_failed_runs.append(line)
 
-    if len(data_to_extract) < len(list_unsucessful_flux_labels):
-        list_not_found = [run[:-1] for run in list_unsucessful_flux_labels if not any(fnmatch.fnmatch(line.strip().split(' ')[0],run) for line in data_to_extract)]
-        list_not_found = [float(run) for run in list_not_found]
-        print('Up to this point extracted data:')
-        for item in data_to_extract:
-            print(item, end='')
-        print('Up to this point not found surfaces:')
-        print(list_not_found)
-        print('Could not find all surfaces. Convert to float to handle rounding.') 
+    if len(found_surfaces_of_failed_runs) < len(failed_runs):
+        not_matched_flux_labels = [flux_label[:-1] for flux_label in flux_labels_of_failed_runs if not any(fnmatch.fnmatch(surface.strip().split(' ')[0],flux_label) for surface in found_surfaces_of_failed_runs)]
+        not_matched_flux_labels = [float(flux_label) for flux_label in not_matched_flux_labels]
+        print('Up to this point found '+ str(len(found_surfaces_of_failed_runs)) + ' surfaces:')
+        for surface in found_surfaces_of_failed_runs:
+            print(surface, end='')
+        print('Up to this point not matched ' + str(len(not_matched_flux_labels)) + ' flux_labels:')
+        for flux_label in not_matched_flux_labels:
+            print(flux_label)
+        print('Could not find all surfaces. Convert to float to handle rounding and scientific notation format in surfaces.dat.') 
         with open('surfaces.dat', 'r') as surface_file:
             for line in surface_file:
                 columns = line.strip().split(' ')
-                flux_label = float(columns[0])
+                flux_label_of_line = float(columns[0])
 
-                if abs(flux_label) < eps:
+                if abs(flux_label_of_line) < eps:
                     reference = 1 # compare absolut difference
                 else:
-                    reference = flux_label # compare relative difference
+                    reference = flux_label_of_line # compare relative difference
 
-                if any(abs(flux_label - run)/reference < eps for run in list_not_found):
-                    data_to_extract.append(line)
+                for flux_label in not_matched_flux_labels:
+                    if abs(flux_label_of_line - flux_label)/reference < eps:
+                        found_surfaces_of_failed_runs.append(line)
+                        not_matched_flux_labels.remove(flux_label)
+                        break
 
-        if len(data_to_extract) < len(list_unsucessful_flux_labels):
-            print('Could not find all surfaces, considering a relative tolerance of .' + eps)
-            print('Possibly increase tolerance eps or check surface.dat for not agreeing case.')
-            print('List of not found surfaces:')
-            print(list_not_found)
+        if len(found_surfaces_of_failed_runs) < len(failed_runs):
+            print('Could not find all surfaces, considering a relative tolerance of ' + str(eps))
+            print('Possibly increase tolerance eps or check surfaces.dat for not agreeing case.')
+            print('List of not matched flux_labels:')
+            print(not_matched_flux_labels)
     
-    if len(data_to_extract) > len(list_unsucessful_flux_labels):
+    if len(found_surfaces_of_failed_runs) > len(failed_runs):
         print('Data found corresponding to the not successfull runs:')
-        print(data_to_extract)
+        print(found_surfaces_of_failed_runs)
         raise ValueError('Something went wrong. More surfaces found than failed runs.')
 
-    print('Found in total ' + str(len(data_to_extract)) + ' surfaces in surfaces.dat for failed runs.')
+    print('Found in total ' + str(len(found_surfaces_of_failed_runs)) + ' matches between the surfaces in surfaces.dat and the ' + str(len(failed_runs)) + ' failed runs.')
 
-    for item in data_to_extract:
-        print(item, end='')
+    for surface in found_surfaces_of_failed_runs:
+        print(surface, end='')
 
     with open('surfaces_failed.dat', 'w') as file:
-        file.writelines(data_to_extract)
+        file.writelines(found_surfaces_of_failed_runs)
 
     # Make submit file for failed runs
-    append_list_unsucessful_runs_par(list_unsucessful_runs,"submit_failed.template", "submit_failed")
+    append_list_failed_runs_par(failed_runs,"submit_failed.template", "submit_failed")
 
     # Make surfaces folders for failed runs
     print('Executing create_surf.py to generate new folders of failed runs:')
@@ -248,8 +255,8 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Setup retry of failed runs for PAR version.')
     parser.add_argument('--faulty_file_pattern', type=str, default="propagator*.h5", help='Name of the file which PRESENCE indicates a failure.')
-    parser.add_argument('--use_failed_record', type=bool, default=False, help='If True, the list of failed runs is read from record_of_failed_runs.txt')
-    parser.add_argument('--eps', type=float, default=1e-10, help='Allowed (relative) rounding difference between folder name and flux label in surfaces.dat.')
+    parser.add_argument('--use_record_of_failed_runs', type=bool, default=False, help='If True, the list of failed runs is read from record_of_failed_runs.txt')
+    parser.add_argument('--eps', type=float, default=1e-09, help='Allowed (relative) rounding difference between folder name and flux label in surfaces.dat.')
     parser.add_argument('--no_sideeffects', type=bool, default=False, help='If True, only print list of failed runs. No sideeffects.')
     args = parser.parse_args()
-    setup_retry_of_failed_runs_PAR_VERSION(args.faulty_file_pattern, args.use_failed_record, args.eps, args.no_sideeffects)
+    setup_retry_of_failed_runs_PAR_VERSION(args.faulty_file_pattern, args.use_record_of_failed_runs, args.eps, args.no_sideeffects)
