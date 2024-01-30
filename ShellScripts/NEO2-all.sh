c826cdfcc9142c74fad4f3d667cb7ad10ea66d3e
GeorgGrassler <georg.grassler@tugraz.at>
2024-01-30 16:46:22 +0100

    Added shell script for executing all stages /reconstruction runs NEO-2 PAR
    The original NEO2-all.sh that is mentioned in
    DOC/ExtraDocuments/neo2-par-eccd_runs.pdf is not longer accesable, so this
    new version was created. An additional example condor_submit file that can
    use this new shell script, follows.

---
+++ ShellScripts/NEO2-all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#cd $1
+#echo $1 $2 $3
+#source /afs/itp.tugraz.at/opt/intel/12.1/mkl/bin/mklvars.sh intel64
+# Manual definition
+CPUs=$1
+# CPUs via Condor
+# CPUs=$(awk '/^Cpus = [[:digit:]]+$/ {print $3}' $_CONDOR_MACHINE_AD)
+echo "Job requested $CPUs CPUs, starting mpirun..."
+sed -i "s#prop_reconstruct.*=.*#prop_reconstruct = 0#" neo2.in
+echo "Reconstruct 0 - Calculate Greensfunctions of ripples"
+OMP_NUM_THREADS=2 mpirun.openmpi -mca orte_tmpdir_base "/tmp/" -np $CPUs ./neo_2.x
+sed -i "s#prop_reconstruct.*=.*#prop_reconstruct = 1#" neo2.in
+echo "Reconstruct 1 - Solve for boundary condition on fieldline start"
+OMP_NUM_THREADS=2 ./neo_2.x
+sed -i "s#prop_reconstruct.*=.*#prop_reconstruct = 2#" neo2.in
+echo "Reconstruct 2 - Calculate the solution g(eta,phi) at the interpolation grid knots."
+OMP_NUM_THREADS=2 mpirun.openmpi -mca orte_tmpdir_base "/tmp/" -np $CPUs ./neo_2.x
+sed -i "s#prop_reconstruct.*=.*#prop_reconstruct = 3#" neo2.in
+echo "Reconstruct 3 - Save results to .h5 and remove individual files"
+OMP_NUM_THREADS=2 ./neo_2.x
+#cd PLOTS
+#./neo2_g.x
+#cd ..
+echo "Job done."
