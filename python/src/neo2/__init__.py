579d8fecb58a362914ab5ed8208de65921e1faf2
GeorgGrassler <georg.grassler@tugraz.at>
2024-02-14 18:39:04 +0100

    setup test sqrt_spol conversion for python module

--- python/src/neo2/__init__.py
+++ python/src/neo2/__init__.py
@@ -5,4 +5,4 @@ neo2
 This is the package containing the modules offered by the setup functions for NEO-2
 """
 
-from mars_input_to_neo2_ql_input import convert_sqrt_spol_to_sqrt_stor
+from mars_input_to_neo2_ql_input import convert_sqrt_spol_to_sqrt_stor, get_mars_input_profiles
