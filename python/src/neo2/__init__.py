f0f2163f294e862c9991f11a4622f8a38b0fc380
GeorgGrassler <georg.grassler@tugraz.at>
2024-02-14 18:07:57 +0100

    initialized python module

---
+++ python/src/neo2/__init__.py
@@ -0,0 +1,8 @@
+"""
+neo2
+-----------
+
+This is the package containing the modules offered by the setup functions for NEO-2
+"""
+
+from mars_input_to_neo2_ql_input import convert_sqrt_spol_to_sqrt_stor
