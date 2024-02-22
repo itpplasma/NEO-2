d6279c9a478a9395cc07dfe45725089cb9ddce57
GeorgGrassler <georg.grassler@tugraz.at>
2024-02-22 18:28:56 +0100

    fixed bug: wrong array orientation for neo-2 input

--- python/src/neo2_mars/plot_neo2_ql_input_profiles.py
+++ python/src/neo2_mars/plot_neo2_ql_input_profiles.py
@@ -29,28 +29,28 @@ def read_input_species(input_neo2):
 
 def make_figure_of_input_profiles_neo2_ql(input_profiles):
     figure, axes = plt.subplots(4, 2, figsize=(10, 5 * 4))
-    add_profiles_to_figure(input_profiles, axes)
+    add_profiles_to_figure(input_profiles, axes,color='b')
     return figure, axes
 
-def add_profiles_to_figure(input_profiles, figure_axes):
+def add_profiles_to_figure(input_profiles, figure_axes,color):
     row = 0
     col = 0
     for profile_name in input_profiles:
         if profile_name not in ['boozer_s', 'species']:
             if input_profiles[profile_name].ndim == 1:
-                add_one_species_profile_to_axes(input_profiles, profile_name, figure_axes[row, col])
+                add_one_species_profile_to_axes(input_profiles, profile_name, figure_axes[row, col],color)
             else:
-                add_multiple_species_profiles_to_axes(input_profiles, profile_name, figure_axes[row, col])
+                add_multiple_species_profiles_to_axes(input_profiles, profile_name, figure_axes[row, col],color)
             set_axes_properties(figure_axes[row, col], profile_name)
             row, col = update_row_col(row, col)
     return figure_axes
 
-def add_multiple_species_profiles_to_axes(input_profiles, profile_name, axes):
+def add_multiple_species_profiles_to_axes(input_profiles, profile_name, axes,color):
     for species in input_profiles['species']['tag']:
-        axes.plot(input_profiles['boozer_s'], input_profiles[profile_name][species-1])
+        axes.plot(input_profiles['boozer_s'], input_profiles[profile_name][species-1],color=color)
 
-def add_one_species_profile_to_axes(input_profiles, profile_name, axes):
-    axes.plot(input_profiles['boozer_s'], input_profiles[profile_name])
+def add_one_species_profile_to_axes(input_profiles, profile_name, axes,color):
+    axes.plot(input_profiles['boozer_s'], input_profiles[profile_name],color=color)
 
 def set_axes_properties(axes, profile_name):
     axes.set_xlabel('s_tor')
@@ -70,12 +70,25 @@ def update_row_col(row, col):
 
 ########################################################################################
 
-input_profiles_h5filename = "/temp/grassl_g/TEST_NTV_DEMO/multi_spec_demo.in"
+mars_input_profiles_h5filename = "/temp/grassl_g/TEST_NTV_DEMO/multi_spec_demo_MARS.in"
 control_input_profiles_h5filename = "/temp/grassl_g/TEST_NTV_DEMO/control_profile.in"
+astra_input_profiles_h5filename = "/temp/grassl_g/TEST_NTV_DEMO/multi_spec_demo_ASTRA.in"
+astra2_input_profiles_h5filename = "/temp/grassl_g/TEST_NTV_DEMO/multi_spec_demo_ASTRA2.in"
+chease_input_profiles_h5filename = "/temp/grassl_g/TEST_NTV_DEMO/multi_spec_demo_CHEASE.in"
+temp_buchholz_paper_input_profiles_h5filename = "/proj/plasma/DATA/DEMO/teams/Equilibrium_DEMO2019_CHEASE/multi_spec_demo_version_1_2_equidistant_s.in"
 
-input_profiles = get_input_profiles_neo2_ql(input_profiles_h5filename)
+mars_input_profiles = get_input_profiles_neo2_ql(mars_input_profiles_h5filename)
 control_input_profiles = get_input_profiles_neo2_ql(control_input_profiles_h5filename)
+astra_input_profiles = get_input_profiles_neo2_ql(astra_input_profiles_h5filename)
+astra2_input_profiles = get_input_profiles_neo2_ql(astra2_input_profiles_h5filename)
+chease_input_profiles = get_input_profiles_neo2_ql(chease_input_profiles_h5filename)
+temp_buchholz_paper_input_profiles = get_input_profiles_neo2_ql(temp_buchholz_paper_input_profiles_h5filename)
 #control_input_profiles['boozer_s'] = control_input_profiles['boozer_s']**2
 
-profile_figure, profile_axes = make_figure_of_input_profiles_neo2_ql(input_profiles)
-profile_axes = add_profiles_to_figure(control_input_profiles, profile_axes)
+profile_figure, profile_axes = make_figure_of_input_profiles_neo2_ql(mars_input_profiles)
+#profile_axes = add_profiles_to_figure(control_input_profiles, profile_axes,color='r')
+#profile_axes = add_profiles_to_figure(astra_input_profiles, profile_axes,color='g')
+#profile_axes = add_profiles_to_figure(astra2_input_profiles, profile_axes,color='y')
+#profile_axes = add_profiles_to_figure(chease_input_profiles, profile_axes,color='c')
+profile_axes = add_profiles_to_figure(temp_buchholz_paper_input_profiles, profile_axes,color='m')
+plt.show()
