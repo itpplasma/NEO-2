0df902c385e58efbdcdca004ad8108919986d852
GeorgGrassler <georg.grassler@tugraz.at>
2024-04-30 17:41:09 +0200

    refactored load_profiles_data.py

--- python/src/neo2_mars/load_profile_data.py
+++ python/src/neo2_mars/load_profile_data.py
@@ -1,26 +1,27 @@
 import numpy as np
 
-def get_profiles_over_equidist_grid(src:dict, equidist_grid:str, options:dict={}):
+def load_profiles_and_interp(src:dict, equidist_grid:str, options:dict={}):
 
     options = set_default_options_where_missing(options)
-    profile = load_profile(src)
+
+    profiles, sqrtspol, sqrtstor = load_profiles(src)
     if options['is_not_SI']:
-        profile = convert_units_to_SI(profile)
+        profiles = convert_units_to_SI(profiles)
 
     if equidist_grid == 'sqrtspol':
-        profile['sqrtspol'], profile['sqrtstor'] = interp_to_equidist_s(profile['sqrtspol'], profile['sqrtstor'], options)
+        sqrtspol, sqrtstor = interp_y_to_equidist_s(s=sqrtspol, y=sqrtstor, options=options)
     elif equidist_grid == 'sqrtstor':
-        profile['sqrtstor'], profile['sqrtspol'] = interp_to_equidist_s(profile['sqrtstor'], profile['sqrtspol'], options)
+        sqrtstor, sqrtspol = interp_y_to_equidist_s(s=sqrtstor, y=sqrtspol, options=options)
     elif equidist_grid == 'spol':
-        spol, profile['sqrtstor'] = interp_to_equidist_s(profile['sqrtspol']**2, profile['sqrtstor'], options)
-        profile['sqrtspol'] = np.sqrt(spol)
+        spol, sqrtstor = interp_y_to_equidist_s(s=sqrtspol**2, y=sqrtstor, options=options)
+        sqrtspol = np.sqrt(spol)
 
-    profile = interp_profile(profile, profile['sqrtspol']**2)
+    profiles = interp_profiles(profiles, sqrtspol)
 
-    return profile
+    return profiles, sqrtspol, sqrtstor
 
 def set_default_options_where_missing(options):
-    default_options = {'n_s': 30, 'min_s': 0.0, 'max_s': 1.0, 'is_not_SI': False}
+    default_options = {'n_s': 10, 'min_s': 0.0, 'max_s': 1.0, 'is_not_SI': False}
     for key in default_options:
         if key not in options:
             options[key] = default_options[key]
@@ -28,38 +29,45 @@ def set_default_options_where_missing(options):
     options['min_s'] = max(options['min_s'], 0.0)
     return options
 
-def load_profile(source):
+def load_profiles(src):
     profiles = {}
-    s = {}
-    for key in source:
-        data = np.loadtxt(source[key]['filename'])
-        if key == 'sqrtspol' or key == 'sqrtstor':
-            s[key] = data[:,source[key]['column']]
-        else:
-            profiles[key] = data[:,source[key]['column']]
-    return profiles, s
+    for profile in src:
+        data = np.loadtxt(src[profile]['filename'])
+        if profile != 'sqrtspol' and profile != 'sqrtstor':
+            profiles[profile] = data[:,src[profile]['column']]
+    data = np.loadtxt(src['sqrtspol']['filename'])
+    sqrtspol = data[:,src['sqrtspol']['column']]
+    sqrtstor = data[:,src['sqrtstor']['column']]
+    profiles['sqrtspol'] = sqrtspol.copy()
+    return profiles, sqrtspol, sqrtstor
 
 def convert_units_to_SI(profiles):
     keV2eV = 1.0e3
     krads2rads = 1.0e3
     norm_density2si_density = 1.0e+19
-    profiles['electron_temperature'] *= keV2eV
-    profiles['ion_temperature'] *= keV2eV
-    profiles['rotation_velocity'] /= profiles['major_radius']
-    profiles['rotation_velocity'] *= krads2rads
-    profiles['electron_density'] *= norm_density2si_density
+    profiles['Te'] *= keV2eV
+    profiles['Ti'] *= keV2eV
+    profiles['vrot'] /= profiles['major_radius']
+    profiles['vrot'] *= krads2rads
+    profiles['ne'] *= norm_density2si_density
 
-def interp_to_equidist_s(s, y, options):
+def interp_y_to_equidist_s(s, y, options):
     equidist_s = np.linspace(options['min_s'], options['max_s'], options['n_s'])
-    equidist_y = np.interp(equidist_s, s, y)
+    equidist_y = interp_cubic(equidist_s, s, y)
     if options['max_s'] == 1.0:
         equidist_y[-1] = 1.0
     if options['min_s'] == 0.0:
         equidist_y[0] = 0.0
     return equidist_s, equidist_y
 
-def interp_profile(profiles, spol):
-    for key in profiles:
-        if key != 'sqrtspol' or key != 'sqrtstor':
-            profiles[key] = np.interp(spol, profiles['sqrtspol']**2, profiles[key])
+def interp_profiles(profiles, sqrtspol):
+    for profile in profiles:
+        if profile != 'sqrtspol':
+            profiles[profile] = interp_cubic(sqrtspol, profiles['sqrtspol'], profiles[profile])
+    del profiles['sqrtspol']
     return profiles
+
+def interp_cubic(x_new, x , y):
+    from scipy.interpolate import interp1d
+    f = interp1d(x, y, kind='cubic')
+    return f(x_new)
