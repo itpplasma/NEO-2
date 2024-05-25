9002b41a596295a0aa85131fcc3e3341d70d81ba
GeorgGrassler <georg.grassler@tugraz.at>
2024-05-25 13:05:40 +0200

    clean: renaming

--- python/src/neo2_mars/__init__.py
+++ python/src/neo2_mars/__init__.py
@@ -4,9 +4,10 @@ neo2_mars
 
 This is the package containing tools to setup NEO2-QL runs from MARS input files.
 """
-from .mars_input_to_neo2_ql_input import write_neo2_input_profiles_from_mars
-from .mars_input_to_neo2_ql_input import get_profiles_mars
-from .mars_input_to_neo2_ql_input import get_sqrtstor_profile 
-from .mars_input_to_neo2_ql_input import get_mars_q_over_equidist_spol, mars_sqrtspol2sqrtstor
-from .mars_input_to_neo2_ql_input import mars_sqrtspol2stor
-from .run_generate_multispec_input_from_mars import run_generate_multispec_input
+from .mars_profiles_to_neo2_ql_profiles import write_neo2_input_profiles_from_mars
+from .mars_profiles_to_neo2_ql_profiles import get_profiles_mars
+from .mars_profiles_to_neo2_ql_profiles import get_sqrtstor_profile 
+from .mars_profiles_to_neo2_ql_profiles import get_mars_q_over_equidist_spol, mars_sqrtspol2sqrtstor
+from .mars_profiles_to_neo2_ql_profiles import mars_sqrtspol2stor
+from .run_generate_multispec_input_from_mars import generate_multispec_input_from_mars
+from .run_generate_multispec_input_from_mars import get_species_cgs_form_mars
--- python/src/neo2_mars/run_generate_multispec_input_from_mars.py
+++ python/src/neo2_mars/run_generate_multispec_input_from_mars.py
@@ -3,8 +3,12 @@ import numpy as np
 
 # Homebrew imports
 from neo2_ql import generate_multispec_input
+from .mars_profiles_to_neo2_ql_profiles import write_neo2_input_profiles_from_mars
 
-def run_generate_multispec_input(path_to_input_files, number_of_surfaces, bounds):
+def generate_multispec_input_from_mars(mars_dir: str, output_file: str='multi_spec.in',
+                                       number_of_surfaces: int=10, bounds: list=[0, 1]):
+    write_neo2_input_profiles_from_mars(mars_dir, '/tmp/')
+
     hdf5FileName = 'multi_spec_demo.in'
     path_to_input_files = './input_files_for_generate_neo2_profile/'
     
@@ -28,3 +32,13 @@ def run_generate_multispec_input(path_to_input_files, number_of_surfaces, bounds
     switch_grid = 2
     generate_multispec_input(hdf5FileName, path_to_input_files, data_source, species_definition, isw_Vphi_loc,
                              species_tag_Vphi, input_unit_type, bounds, switch_grid)
+
+def get_species_cgs_form_mars(mars_dir):
+    import f90nml
+    PROTON_MASS_CGS = 1.6726219e-24
+    run_config = f90nml.read(os.path.join(mars_dir, 'RUN.IN'))
+    Z = run_config['KINETIC']['ESPECIES_Z']
+    Ze, Zi = Z[Z < 0], Z[Z > 0]
+    mass_numbers = run_config['KINETIC']['ESPECIES_M']
+    me, mi = (mass_numbers[Z.index(Ze)], mass_numbers[Z.index(Zi)]) * PROTON_MASS_CGS
+    return Ze, Zi, me, mi
