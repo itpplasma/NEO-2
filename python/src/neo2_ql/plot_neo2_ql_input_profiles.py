ecf6e7f52344ce06bdf3b566c201bb709b0d7618
GeorgGrassler <georg.grassler@tugraz.at>
2024-06-04 13:51:11 +0200

    tested mars-neo-2 input generation

--- python/src/neo2_ql/plot_neo2_ql_input_profiles.py
+++ python/src/neo2_ql/plot_neo2_ql_input_profiles.py
@@ -5,31 +5,31 @@ import h5py
 
 ########################################################################################
 
-def get_neo2_ql_multispec_profiles(multispec_filename: str):
-    multispec = h5py.File(multispec_filename,"r",)
-    profiles = read_profiles(multispec)
-    profiles['species'] = read_species(multispec)
-    return profiles
+def get_neo2_ql_input_profiles(input_hdf5: str):
+    inputs = h5py.File(input_hdf5,"r",)
+    profiles = read_profiles(inputs)
+    species = read_species(inputs)
+    return profiles, species
 
-def read_profiles(multispec):
+def read_profiles(inputs):
     profiles = {}
     profiles_names = ['T_prof', 'dT_ov_ds_prof', 'n_prof', 'dn_ov_ds_prof', 'kappa_prof', 'Vphi', 'rho_pol']
     for name in profiles_names:
         profiles[name] = {}
-        profiles[name]['x'] = np.array(multispec['boozer_s'])
-        profiles[name]['y'] = np.array(multispec[name])
+        profiles[name]['x'] = np.array(inputs['boozer_s'])
+        profiles[name]['y'] = np.array(inputs[name])
     return profiles
 
-def read_species(multispec):
+def read_species(inputs):
     species = {}
-    species['tag'] = np.array(multispec['species_tag'])
-    species['charge'] = np.array(multispec['species_def'])[0][:,0]
-    species['mass'] = np.array(multispec['species_def'])[1][:,0]
+    species['tag'] = np.array(inputs['species_tag'])
+    species['charge'] = np.array(inputs['species_def'])[0][:,0]
+    species['mass'] = np.array(inputs['species_def'])[1][:,0]
     return species
 
 ########################################################################################
 
-def make_figure_neo2_ql_multispec_profiles(profiles, color: str='b', label: str=''):
+def make_figure_neo2_ql_input_profiles(profiles, color: str='b', label: str=''):
     fig, axes = plt.subplots(4, 2, figsize=(10, 5 * 4))
     add_profiles_to_axes(profiles, axes, color=color, label=label)
     return fig, axes
@@ -38,16 +38,15 @@ def add_profiles_to_axes(profiles, axes, color, label):
     row = 0
     col = 0
     for profile in profiles.keys():
-        if profile not in ['boozer_s', 'species']:
-            if profiles[profile]['y'].ndim == 1:
-                add_one_species_profile_to_ax(profiles[profile], axes[row, col], color)
-            else:
-                add_multiple_species_profiles_to_ax(profiles[profile], axes[row, col], color)
-            set_axes_properties(axes[row, col], profile, color=color, label=label)
-            col += 1
-            if col == 2:
-                col = 0
-                row += 1
+        if profiles[profile]['y'].ndim == 1:
+            add_one_species_profile_to_ax(profiles[profile], axes[row, col], color)
+        else:
+            add_multiple_species_profiles_to_ax(profiles[profile], axes[row, col], color)
+        set_axes_properties(axes[row, col], profile, color=color, label=label)
+        col += 1
+        if col == 2:
+            col = 0
+            row += 1
     return axes
 
 def add_multiple_species_profiles_to_ax(profiles, ax, color):
@@ -66,8 +65,8 @@ def set_axes_properties(ax, profile, color, label):
 
 ########################################################################################
 
-eps_2023_multispec = "/proj/plasma/DATA/DEMO/teams/Equilibrium_DEMO2019_CHEASE/multi_spec_demo_version_1_2_equidistant_s.in"
-
-eps_2023_profiles = get_neo2_ql_multispec_profiles(eps_2023_multispec)
-fig, axes = make_figure_neo2_ql_multispec_profiles(eps_2023_profiles, label='EPS 2023')
-plt.show()
+if __name__ == '__main__':
+    eps_2023_input = "/proj/plasma/DATA/DEMO/teams/Equilibrium_DEMO2019_CHEASE/multi_spec_demo_version_1_2_equidistant_s.in"
+    eps_2023_profiles, _ = get_neo2_ql_input_profiles(eps_2023_input)
+    fig, axes = make_figure_neo2_ql_input_profiles(eps_2023_profiles, label='EPS 2023')
+    plt.show()
