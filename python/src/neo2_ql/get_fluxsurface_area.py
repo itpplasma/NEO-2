76fc94492c88d76b3d81db5481162926eafaed45
GeorgGrassler <georg.grassler@tugraz.at>
2024-12-06 19:17:10 +0100

    get average nabla stor

--- python/src/neo2_ql/get_fluxsurface_area.py
+++ python/src/neo2_ql/get_fluxsurface_area.py
@@ -1,6 +1,15 @@
 # %%
+from .get_integral_ntv import get_volume_element
+
 def main():
     return
+
+def get_average_nabla_stor(output_filename):
+    import h5py
+    with h5py.File(output_filename) as file:
+        stor = file["boozer_s"][:]
+        average_nabla_stor = file["avnabpsi"][:]
+    return stor, average_nabla_stor
 
 if __name__=="__main__":
     main()
--- python/test/test_get_fluxsurface_area.py
+++ python/test/test_get_fluxsurface_area.py
@@ -8,10 +8,18 @@ def neo2_output():
     clean_up(filename)
     n_surf = 10
     content = {}
-    content["avnabpsi"] = np.linspace(0.1,1,n_surf)
+    content["boozer_s"] = np.linspace(0, 1, n_surf)
+    content["avnabpsi"] = np.linspace(0.1, 1, n_surf)
+    content["avbhat2"] = np.linspace(1, 0.91, n_surf)
+    content["Bref"] = np.linspace(0.1, 1, n_surf)
+    content["psi_pr_hat"] = np.linspace(0.1, 1, n_surf)
+    content["bcovar_tht"] = -np.linspace(1, 0.1, n_surf)
+    content["bcovar_phi"] = -np.linspace(0.1, 1, n_surf)
+    content["aiota"] = np.linspace(1, 0.1, n_surf)
     with h5py.File(filename, "w") as file:
-        average_nabla_stor = file.create_dataset("avnabpsi",(n_surf,), dtype="f")
-        average_nabla_stor[:] = content["avnabpsi"]
+        for name in content.keys():
+            dataset = file.create_dataset(name,(n_surf,), dtype="f")
+            dataset[:] = content[name]
     yield filename, content
     clean_up(filename)
 
@@ -21,6 +29,16 @@ def clean_up(filename):
         os.remove(filename)
 
 def test_read_neo2_output(neo2_output):
-    filename , content = neo2_output
+    filename, content = neo2_output
     with h5py.File(filename, "r") as file:
-        assert np.allclose(content["avnabpsi"], file["avnabpsi"][:])
+        for name in content.keys():
+            assert np.allclose(content[name], file[name][:])
+
+def test_get_average_nable_stor(neo2_output):
+    from neo2_ql import get_average_nabla_stor
+    filename, content = neo2_output
+    stor, average_nabla_stor = get_average_nabla_stor(filename)
+    assert np.allclose(stor, content["boozer_s"])
+    assert np.allclose(average_nabla_stor, content["avnabpsi"])
+
+    
