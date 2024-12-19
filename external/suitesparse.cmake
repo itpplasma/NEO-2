b5bdd1dff615c73836ef4e9baff503253b3d3d75
Christopher Albert <albert@tugraz.at>
2024-12-19 18:03:47 +0100

    Adding SuiteSparse build

--- external/suitesparse.cmake
+++ external/suitesparse.cmake
@@ -1,7 +1,9 @@
 FetchContent_Declare(
     SuiteSparse
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
     GIT_REPOSITORY https://github.com/DrTimothyAldenDavis/SuiteSparse.git
     GIT_TAG v7.6.1
     GIT_SHALLOW TRUE
-    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 )
+
+FetchContent_MakeAvailable(SuiteSparse)
