431ddbf6a9b2b6cc8f12d2adaad0210f35461cb7
Christopher Albert <albert@tugraz.at>
2024-12-19 17:52:39 +0100

    Adapt external

--- external/fgsl.cmake
+++ external/fgsl.cmake
@@ -1,11 +1,17 @@
-FetchContent_Declare(
+include(ExternalProject)
+
+ExternalProject_Add(
     FGSL
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-    URL "https://github.com/reinh-bader/fgsl/archive/refs/tags/v1.6.0.tar.gz"
-    CONFIGURE_COMMAND "mkdir m4 && autoreconf -i && ./configure"
+    PREFIX ${CMAKE_BINARY_DIR}/fgsl
+    URL https://github.com/reinh-bader/fgsl/archive/refs/tags/v1.6.0.tar.gz
+    CONFIGURE_COMMAND mkdir m4 && autoreconf -i && ./configure
     BUILD_COMMAND make
+    INSTALL_COMMAND ""
+    BUILD_IN_SOURCE 1
     BUILD_BYPRODUCTS
-        ${CMAKE_BINARY_DIR}/_deps/fgsl-build/src/FGSL/.libs/libfgsl${CMAKE_STATIC_LIBRARY_SUFFIX}
+       ${CMAKE_BINARY_DIR}/fgsl/src/FGSL/.libs/libfgsl${CMAKE_STATIC_LIBRARY_SUFFIX}
 )
 
-FetchContent_MakeAvailable(FGSL)
+add_custom_target(external_fgsl DEPENDS FGSL)
+add_dependencies(external_fgsl FGSL)
--- external/libneo.cmake
+++ external/libneo.cmake
@@ -1,13 +1,15 @@
+include(FetchContent)
+
 FetchContent_Declare(
     LIBNEO
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
     GIT_REPOSITORY https://github.com/itpplasma/libneo.git
     GIT_TAG 14-user-friendly-installation
     BUILD_BYPRODUCTS
-        ${CMAKE_BINARY_DIR}/_deps/libneo-build/src/LIBNEO/build/libneo${CMAKE_SHARED_LIBRARY_SUFFIX}
-        ${CMAKE_BINARY_DIR}/_deps/libneo-build/libneo/src/LIBNEO/build/libmagfie${CMAKE_SHARED_LIBRARY_SUFFIX}
-        ${CMAKE_BINARY_DIR}/_deps/libneo-build/libneo/src/LIBNEO/build/src/MyMPILib/libMyMPILib${CMAKE_STATIC_LIBRARY_SUFFIX}
-        ${CMAKE_BINARY_DIR}/_deps/libneo-build/libneo/src/LIBNEO/build/src/hdf5_tools/libhdf5_tools${CMAKE_STATIC_LIBRARY_SUFFIX}
+        ${CMAKE_BINARY_DIR}/libneo${CMAKE_SHARED_LIBRARY_SUFFIX}
+        ${CMAKE_BINARY_DIR}/libmagfie${CMAKE_SHARED_LIBRARY_SUFFIX}
+        ${CMAKE_BINARY_DIR}/_deps/libneo-build/src/MyMPILib/libMyMPILib${CMAKE_STATIC_LIBRARY_SUFFIX}
+        ${CMAKE_BINARY_DIR}/_deps/libneo-build/hdf5_tools/libhdf5_tools${CMAKE_STATIC_LIBRARY_SUFFIX}
 )
 
 FetchContent_MakeAvailable(LIBNEO)
