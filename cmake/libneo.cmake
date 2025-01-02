9e77cca5d02e2c2afbeaa717c7957502c1890ec6
Christopher Albert <albert@alumni.tugraz.at>
2025-01-02 13:31:50 +0100

    Update libneo.cmake

--- cmake/libneo.cmake
+++ cmake/libneo.cmake
@@ -4,7 +4,6 @@ FetchContent_Declare(
     LIBNEO
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
     GIT_REPOSITORY https://github.com/itpplasma/libneo.git
-    GIT_TAG 14-user-friendly-installation
     BUILD_BYPRODUCTS
         ${CMAKE_BINARY_DIR}/libneo${CMAKE_SHARED_LIBRARY_SUFFIX}
         ${CMAKE_BINARY_DIR}/libmagfie${CMAKE_SHARED_LIBRARY_SUFFIX}
