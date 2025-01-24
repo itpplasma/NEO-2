247220a4b88b6fbe7796acee180eb861bc467360
Christopher Albert <albert@tugraz.at>
2025-01-24 11:37:10 +0100

    Static libneo

--- cmake/libneo.cmake
+++ cmake/libneo.cmake
@@ -5,8 +5,8 @@ FetchContent_Declare(
     DOWNLOAD_EXTRACT_TIMESTAMP TRUE
     GIT_REPOSITORY https://github.com/itpplasma/libneo.git
     BUILD_BYPRODUCTS
-        ${CMAKE_BINARY_DIR}/libneo${CMAKE_SHARED_LIBRARY_SUFFIX}
-        ${CMAKE_BINARY_DIR}/libmagfie${CMAKE_SHARED_LIBRARY_SUFFIX}
+        ${CMAKE_BINARY_DIR}/libneo${CMAKE_STATIC_LIBRARY_SUFFIX}
+        ${CMAKE_BINARY_DIR}/libmagfie${CMAKE_STATIC_LIBRARY_SUFFIX}
         ${CMAKE_BINARY_DIR}/_deps/libneo-build/src/MyMPILib/libMyMPILib${CMAKE_STATIC_LIBRARY_SUFFIX}
         ${CMAKE_BINARY_DIR}/_deps/libneo-build/hdf5_tools/libhdf5_tools${CMAKE_STATIC_LIBRARY_SUFFIX}
     OVERRIDE_FIND_PACKAGE TRUE
